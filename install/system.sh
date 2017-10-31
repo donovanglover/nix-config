e0ad3949dae2bcf230e9d6269baa066c43d6e74b
Donovan Glover <donovan@dglover.co>
2017-10-30 22:08:17 -0400

    Add language and sudo settings to system.sh

--- install/system.sh
+++ install/system.sh
@@ -18,20 +18,38 @@
 #
 ##################################################################################
 
-SYSTEMD="/etc/systemd/system.conf"
-PACMAN="/etc/pacman.conf"
-FONTS="/etc/fonts"
+#########################################################################
+# Language settings: used for localization, etc.
+#########################################################################
 
-# Change the default timeout from 90s to 10s, preventing the system from hanging at shutdown
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen      # Use the en_US.UTF-8 locale
+echo "KEYMAP=us"         > /etc/vconsole.conf   # Explicitly set the keymap to US
+echo "LANG=en_US.UTF-8"  > /etc/locale.conf     # Set the language to en_US.UTF-8
+
+#########################################################################
+# Systemd settings: changes how the core system works
+#########################################################################
+
+SYSTEMD="/etc/systemd/system.conf"
+
+# Change the wait time from 90s to 10s, preventing the system from hanging at shutdown
 echo "DefaultTimeoutStartSec=10s" >> $SYSTEMD
 echo "DefaultTimeoutStopSec=10s" >> $SYSTEMD
 
-# Enable colors in pacman (also enables color in pacaur) by uncommenting the Color line
+#########################################################################
+# Pacman settings: used by pacman and pacaur
+#########################################################################
+
+PACMAN="/etc/pacman.conf"
+
+# Enable colors in pacman by uncommenting the Color line
 sed -i '/Color/s/^#//g' $PACMAN
 
 #########################################################################
 # Global font settings: we have to soft link the settings we want here
 #########################################################################
+
+FONTS="/etc/fonts"
 
 # Disable embedded bitmaps for all fonts
 ln -s $FONTS/conf.avail/70-no-bitmaps.conf $FONTS/conf.d
@@ -42,3 +60,10 @@ ln -s $FONTS/conf.avail/10-sub-pixel-rgb.conf $FONTS/conf.d
 # Enable the LCD filter (reduces color fringing)
 ln -s $FONTS/conf.avail/11-lcdfilter-default.conf $FONTS/conf.d 
 
+#########################################################################
+# Sudo settings: allows us to use su without prompting for a password
+# Particularly useful when installing cower and pacaur, etc.
+#########################################################################
+
+# Note that this setting gets changed in the clean up stage of the build process
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
