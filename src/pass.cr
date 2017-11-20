f4cfe5fb5cd86f7c446e0ba52b22582a0e99f9f1
Donovan Glover <donovan@dglover.co>
2017-11-20 18:58:56 -0500

    Add base pass.cr, a simple password manager

---
+++ src/pass.cr
@@ -0,0 +1,29 @@
+##################################################################################
+#
+#    New Start: A modern Arch workflow built with an emphasis on functionality.
+#    Copyright (C) 2017 Donovan Glover
+#    
+#    Pass: A very simple and straight-forward password manager
+#    Copyright (C) 2017 Donovan Glover
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+##################################################################################
+
+# Pass lets you easily retrieve passwords and other sensitive information.
+# Please do not confuse this pass with the standard pass utility.
+
+# Placeholder for the library we create to handle the plain text file format
+require "txt"
+
