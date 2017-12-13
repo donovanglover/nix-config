4b2031df6f14da64f859f3e692f6eb85a3220280
Donovan Glover <donovan@dglover.co>
2017-12-12 23:46:54 -0500

    Move user.js to etc/user.js and add other disabled content

--- etc/user.js
+++ etc/user.js
@@ -36,3 +36,47 @@ user_pref("devtools.toolbox.selectedTool", "inspector");
 
 // Enable scrolling by holding down the middle mouse button
 user_pref("general.autoScroll", true);
+
+// ============================================================================
+// Other disabled content
+// ============================================================================
+
+// Disable web notifications
+user_pref("dom.webnotifications.enabled", false);
+
+// Disable location-aware browsing
+user_pref("geo.enabled", false);
+
+// Disable screen sharing, audio capture, and video capture
+user_pref("media.navigator.video.enabled", false);
+user_pref("media.getusermedia.screensharing.enabled", false);
+user_pref("media.getusermedia.audiocapture.enabled", false);
+
+// Disable speech recognition and synthesis
+user_pref("media.webspeech.recognition.enable", false);
+user_pref("media.webspeech.synth.enabled", false);
+
+// Disable the gamepad API
+user_pref("dom.gamepad.enabled", false);
+
+// Disable the VR API
+user_pref("dom.vr.enabled", false);
+
+// Disable the vibrator API
+user_pref("dom.vibrator.enabled", false);
+
+// Disable other video features
+user_pref("camera.control.face_detection.enabled", false);
+
+// Set the default search engine to DuckDuckGo
+user_pref("browser.search.defaultenginename", "DuckDuckGo");
+user_pref("browser.search.order.1", "DuckDuckGo");
+user_pref("keyword.URL", "https://duckduckgo.com/html/?q=!+");
+
+// Explicitly declare the region for searches
+user_pref("browser.search.countryCode", "US");
+user_pref("browser.search.region", "US");
+user_pref("browser.search.geoip.url", "");
+
+// Always request the english version of a webpage
+user_pref("intl.accept_languages", "en-us, en");
