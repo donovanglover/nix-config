55396d28665e1786ad53050a5d5047c753517413
Donovan Glover <donovan@dglover.co>
2017-12-15 23:59:42 -0500

    Add i3/new_workspace.sh, an easier way to make new workspaces

---
+++ dotfiles/.config/i3/new_workspace.sh
@@ -0,0 +1,22 @@
+##################################################################################
+#
+#    New Start: A modern Arch workflow built with an emphasis on functionality.
+#    Copyright (C) 2017 Donovan Glover
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+##################################################################################
+
+# See: https://unix.stackexchange.com/questions/138425/
+i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | ag '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
