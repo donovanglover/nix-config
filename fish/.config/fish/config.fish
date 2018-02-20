dd6cbe1ae898f9da505d7a0bc1c8c7d82963d8ad
Donovan Glover <donovan@dglover.co>
2018-02-19 23:58:42 -0500

    Make the stow directory the root directory

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 #set -U fish_greeting ""
 #set -U fish_history ""
 #set -U fish_user_paths
-#set -U BROWSER "waterfox"
+#set -x BROWSER "waterfox"
 
 fish_vi_key_bindings
 
@@ -20,3 +20,10 @@ alias t="tree -L 1"
 
 alias todo="rg TODO"
 alias pwd="pwd | sed -e 's!$HOME!~!g'"
+
+# Start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
+end
