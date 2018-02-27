d90a3033121c1c4572b94c2d5b6d3bdaefbfdbe5
Donovan Glover <donovan@dglover.co>
2018-02-27 15:07:50 -0500

    Move ranger, polybar, and w3m to stow

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,11 @@
+# New Start: A modern Arch workflow built with an emphasis on functionality.
+# Copyright (C) 2017 Donovan Glover
+
+# Terminate any previous instances of polybar
+killall -q polybar
+
+# Wait until there are no more polybar instances running
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Start polybar
+polybar main &
