a95416a088722e6779aeacd41a8d9be2e91db585
Donovan Glover <donovan@dglover.co>
2018-02-27 23:04:08 -0500

    Move sxhkd, xinit, and gnupg to stow

--- sxhkd/.config/sxhkd/screenshot.sh
+++ sxhkd/.config/sxhkd/screenshot.sh
@@ -1,3 +1,6 @@
+# New Start: A modern Arch workflow built with an emphasis on functionality.
+# Copyright (C) 2017 Donovan Glover
+
 DATETIME=`date +%F_%H%M%S`
 shotgun ~/Pictures/Screenshots/$DATETIME.png
 feh ~/Pictures/Screenshots/$DATETIME.png --geometry 3200x1800
