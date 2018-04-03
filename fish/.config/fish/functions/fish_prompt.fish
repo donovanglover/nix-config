500983f36919f3f63090e5f3ee6a61f496e699ba
Donovan Glover <donovan@dglover.co>
2018-04-02 20:56:13 -0400

    Fix the fish prompt
    set_color must be normal here to avoid the next character
    not being the default color.

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -9,9 +9,9 @@ function fish_prompt
     end
 
     set_color magenta;  echo -n "$pwd"
-    set_color white;    echo -n " "
+    set_color normal;   echo -n " "
     set_color red;      echo -n "➤"
     set_color green;    echo -n "➤"
     set_color blue;     echo -n "➤"
-    set_color white;    echo -n " "
+    set_color normal;   echo -n " "
 end
