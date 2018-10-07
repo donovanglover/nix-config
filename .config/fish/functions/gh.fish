bf0a00748707ad74b50c3e0e04b5bb0ce9b07383
Donovan Glover <donovan@dglover.co>
2018-10-07 17:59:31 -0400

    Update misc dotfiles
    Image previews in ranger are back, this time using urxvt instead of w3m.
    Also fixed the screenshot utility and cleaned up some other things.

--- .config/fish/functions/gh.fish
+++ .config/fish/functions/gh.fish
@@ -1,8 +1,5 @@
 # Easily cd into the "home" directory of any repository you're
-# working in. Note that this is a function instead of an alias
-# since `external calls` in aliases are not supported by zsh.
-# The current aliases file works with fish and other shells like
-# zsh.
+# working in.
 function gh
     cd (git rev-parse --show-toplevel)
 end
---
+++ .config/sxhkd/screenshot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# New Start: A modern Arch workflow built with an emphasis on functionality.
+# Copyright (C) 2017-2018 Donovan Glover
+
+DATETIME=`date +%F_%H%M%S`
+shotgun ~/$DATETIME.png
+feh ~/$DATETIME.png --geometry 3200x1800 --scale-down
