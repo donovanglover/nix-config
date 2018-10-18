f19dc21649c262eef66fcb8e7e001885ee3a1e25
Donovan Glover <donovan@dglover.co>
2018-10-18 11:00:05 -0400

    Use symlinks with pywal templates
    Instead of manually changing every instance where a program could run to
    use the config in the cache directory, and instead of copying files
    every time pywal is updated, it makes sense to create symbolic links for
    these files instead.
    
    This ensures that the right settings are used even without knowing the
    passed parameters.

--- .config/wal/done.sh
+++ .config/wal/done.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# ===================
+# ====== bspwm ======
+# ===================
+
 # Source the colors from wal
 source "${HOME}/.cache/wal/colors.sh"
 
@@ -9,6 +13,24 @@ bspc config active_border_color   "$color2"
 bspc config focused_border_color  "$color15"
 bspc config presel_feedback_color "$color1"
 
+# =================================================
+# Symlink config files so programs can be started
+# without referencing the cache directly
+#
+# Includes: zathura, termite, dunst
+# =================================================
+
+mkdir -p  "${HOME}/.config/zathura"
+mkdir -p  "${HOME}/.config/termite"
+mkdir -p  "${HOME}/.config/dunst"
+ln -sf    "${HOME}/.cache/wal/zathurarc"  "${HOME}/.config/zathura/zathurarc"
+ln -sf    "${HOME}/.cache/wal/termite"    "${HOME}/.config/termite/config"
+ln -sf    "${HOME}/.cache/wal/dunstrc"    "${HOME}/.config/dunst/dunstrc"
+
+# ===================
+# ====== dunst ======
+# ===================
+
 # Restart dunst with the new color scheme
 pkill dunst
-dunst -conf "${HOME}/.cache/wal/dunstrc" &
+dunst &
