41f46f38f24d872dc9e7d864fdcd978c74fbf7bc
Donovan Glover <donovan@dglover.co>
2018-10-20 20:24:21 -0400

    polybar: Handle dynamic settings through launch.sh
    Since polybar supports include files, we can generate part of the bar
    config with launch.sh and store it in $HOME/.cache. Since launch.sh is
    a shell script, and since bspc is an interface to bspwm, we can also
    change the necessary window manager settings required for different
    bars, or even configure the settings for no bar at all.
    
    A future implementation of launch.sh may allow us to change polybar
    dimensions based on current resolution and whether the screen is HiDPI
    or not, although it probably won't be *too* fancy since I don't need to
    support every potential DPI a user may have.
    
    Alternatively, someone could submit a patch for polybar to make it
    DPI-aware. This is the ideal solution since the bar would be scaled
    naturally based on dpi, instead of having to manually halve things
    ourselves.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,50 @@
 #!/bin/sh
 # New Start: A modern Arch workflow built with an emphasis on functionality.
-# Copyright (C) 2017 Donovan Glover
+# Copyright (C) 2017-2018 Donovan Glover
 
 # Terminate any previous instances of polybar
 killall -q polybar
+
+bspc_config() {
+  bspc config top_padding $1 &
+  bspc config border_width $2 &
+  bspc config window_gap $3 &
+}
+
+# If no bar was specified, we're done here.
+if [ -z "$1" ]; then bspc_config 0 4 0; exit; fi
+
+# Set defaults
+POLYBAR_HEIGHT=80
+HOST_WIDTH=3840
+
+# Make the bar float
+if [ "$1" == "float" ]; then
+  POLYBAR_BORDER_SIZE=8     # Later set to bspwm's border_width
+  POLYBAR_OFFSET_XY=30      # Later set to bspwm's window_gap
+  POLYBAR_WIDTH=$(($HOST_WIDTH - $POLYBAR_OFFSET_XY * 2))
+fi
+
+# Set the bspwm variables
+BSPWM_TOP_PADDING=$(($POLYBAR_HEIGHT + ${POLYBAR_OFFSET_XY:-0} + ${POLYBAR_BORDER_SIZE:-0} * 2))
+BSPWM_BORDER_WIDTH=${POLYBAR_BORDER_SIZE:-8}
+BSPWM_WINDOW_GAP=${POLYBAR_OFFSET_XY:-20}
+
+bspc_config $BSPWM_TOP_PADDING $BSPWM_BORDER_WIDTH $BSPWM_WINDOW_GAP
+
+# Make the polybar config
+mkdir -p $HOME/.cache/polybar
+
+cat >$HOME/.cache/polybar/config <<EOL
+width                       = ${POLYBAR_WIDTH:-3840}
+height                      = $POLYBAR_HEIGHT
+offset-x                    = ${POLYBAR_OFFSET_XY:-0}
+offset-y                    = ${POLYBAR_OFFSET_XY:-0}
+border-size                 = ${POLYBAR_BORDER_SIZE:-0}
+EOL
 
 # Wait until there are no more polybar instances running
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Start polybar
-launch polybar secondary
+launch polybar main
