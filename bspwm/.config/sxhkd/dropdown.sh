3ebc47ca288511c90a8d157425931d9bc18042ed
Donovan Glover <donovan@dglover.co>
2018-11-01 15:56:03 -0400

    bspwm: Add dropdown.sh
    This commit makes it possible to toggle any terminal you want with
    super + backslash. If the terminal doesn't exist already, it will
    be created for you.

---
+++ bspwm/.config/sxhkd/dropdown.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# New Start: A modern Arch workflow built with an emphasis on functionality.
+# Copyright (C) 2018 Donovan Glover
+
+c='URxvt'         # Class
+i='dropdown'      # Instance
+x='urxvtc -name'  # Executable
+
+# Note: This solution will not work with termite since it registers two ids.
+# See https://github.com/thestinger/termite/issues/634 for more information.
+
+id=$(xdo id -n $i)
+
+bspc rule -r $c:$i
+bspc rule -a $c:$i sticky=on state=floating rectangle=3400x1200+220+200
+
+([ -z "$id" ]) && ($x $i) ||
+  ([[ $(xprop -id "$id" | awk '/window state: / {print $3}') == 'Withdrawn' ]] &&
+    xdo show -n $i || xdo hide -n $i)
