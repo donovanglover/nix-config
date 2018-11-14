c98ac74257faddd1b0121d5f03f916b04ada5e1c
Donovan Glover <donovan@dglover.co>
2018-11-14 18:53:09 -0500

    meta: Remove termite
    I removed termite before, but this time I shouldn't need to add it
    again. The kitty terminal emulator supports image output and handles
    fonts better than termite. Plus, true transparency can be used without
    sacrificing images in the terminal.

--- bspwm/.config/wal/done.sh
+++ bspwm/.config/wal/done.sh
@@ -16,16 +16,12 @@ bspc config presel_feedback_color "$color1"
 # =================================================
 # Symlink config files so programs can be started
 # without referencing the cache directly
-#
-# Includes: zathura, termite, dunst
 # =================================================
 
 mkdir -p  "${HOME}/.config/zathura"
-mkdir -p  "${HOME}/.config/termite"
 mkdir -p  "${HOME}/.config/dunst"
 mkdir -p  "${HOME}/.config/kitty"
 ln -sf    "${HOME}/.cache/wal/zathurarc"  "${HOME}/.config/zathura/zathurarc"
-ln -sf    "${HOME}/.cache/wal/termite"    "${HOME}/.config/termite/config"
 ln -sf    "${HOME}/.cache/wal/dunstrc"    "${HOME}/.config/dunst/dunstrc"
 ln -sf    "${HOME}/.cache/wal/kitty.conf" "${HOME}/.config/kitty/kitty.conf"
 
