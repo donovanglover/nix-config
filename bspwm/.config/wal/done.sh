b8e61965c7f50b3baf48e6f01fc85019aa2aec62
Donovan Glover <donovan@dglover.co>
2018-11-15 11:33:26 -0500

    meta: Split zathura config into separate files
    This lets me use my zathura config without relying exclusively on
    pywal being set up (theme set, done.sh ran, cache not cleared).

--- bspwm/.config/wal/done.sh
+++ bspwm/.config/wal/done.sh
@@ -13,19 +13,9 @@ bspc config active_border_color   "$color2"
 bspc config focused_border_color  "$color15"
 bspc config presel_feedback_color "$color1"
 
-# =================================================
-# Symlink config files so programs can be started
-# without referencing the cache directly
-# =================================================
-
-mkdir -p  "${HOME}/.config/zathura"
+# Symlink dunst config
 mkdir -p  "${HOME}/.config/dunst"
-ln -sf    "${HOME}/.cache/wal/zathurarc"  "${HOME}/.config/zathura/zathurarc"
 ln -sf    "${HOME}/.cache/wal/dunstrc"    "${HOME}/.config/dunst/dunstrc"
-
-# ===================
-# ====== dunst ======
-# ===================
 
 # Restart dunst with the new color scheme
 pkill dunst
