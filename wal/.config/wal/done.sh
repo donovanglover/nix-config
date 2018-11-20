186495c97ea59d7525d0e5c735ab75911b6b39c6
Donovan Glover <donovan@dglover.co>
2018-11-19 19:27:54 -0500

    wal: Change bspwm colors
    For most color schemes, color 8 is darker than color 7. This makes it
    easier to distinguish between focused windows.

--- wal/.config/wal/done.sh
+++ wal/.config/wal/done.sh
@@ -8,10 +8,10 @@
 source "${HOME}/.cache/wal/colors.sh"
 
 # Set the border colors
-bspc config normal_border_color   "$color1"
+bspc config normal_border_color   "$color8"
 bspc config active_border_color   "$color2"
-bspc config focused_border_color  "$color15"
-bspc config presel_feedback_color "$color1"
+bspc config focused_border_color  "$color7"
+bspc config presel_feedback_color "$color7"
 
 # Symlink dunst config
 mkdir -p  "${HOME}/.config/dunst"
