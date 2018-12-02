9bdcba18c8d8a78764e9fe333b6d467b6379ab2d
Donovan Glover <donovan@dglover.co>
2018-12-02 17:15:11 -0500

    archlinux: Ask for size of primary partition

--- .archlinux/install-scripts/install.sh
+++ .archlinux/install-scripts/install.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+echo -n "Enter the size (in GiB) to give the primary partition: "
+read DISTSIZE
+
 echo -n "Enter a hostname for this machine: "
 read HOSTNAME
 
 echo -n "Enter a username for the local account with sudo rights: "
 read USERNAME
 
+env DISTSIZE="$DISTSIZE" ./001-preinstall
 env HOSTNAME="$HOSTNAME" ./003-configure
 env USERNAME="$USERNAME" ./004-postinstall
