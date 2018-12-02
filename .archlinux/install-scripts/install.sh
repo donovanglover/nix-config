0c8cfe8cc289c5d706ac0b76d8f45cbf8f60df5e
Donovan Glover <donovan@dglover.co>
2018-12-02 17:32:25 -0500

    archlinux: Ask for confirmation before installation
    This should ensure that a user knows what they're doing before running
    the install scripts. Note that parted will also ask for a confirmation
    before overwriting existing data.

--- .archlinux/install-scripts/install.sh
+++ .archlinux/install-scripts/install.sh
@@ -11,6 +11,21 @@ read HOSTNAME
 echo -n "Enter a username for the local account with sudo rights: "
 read USERNAME
 
+echo "-----------------------------------------------------"
+echo "Arch Linux will be installed with the settings above."
+echo "NOTE: You should not run this script if you do not"
+echo "      understand what it does. Bad things may happen."
+echo -n "Type YES to continue, or Ctrl+C to abort. "
+read CONFIRM
+
+if [ "$CONFIRM" != "YES" ]; then
+  echo "YES was not given, exiting..."
+  exit
+fi
+
+echo "====================================================="
+echo "Running install scripts..."
+
 env DISTSIZE="$DISTSIZE" ./001-preinstall
 env HOSTNAME="$HOSTNAME" ./003-configure
 env USERNAME="$USERNAME" ./004-postinstall
