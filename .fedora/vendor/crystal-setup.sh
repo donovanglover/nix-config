10b8f4f648ccd811a06c1a3e03263f3d5425f4da
Donovan Glover <donovan@dglover.co>
2022-06-11 15:33:09 -0400

    fedora: Remove vendor scripts
    Instead of setting up a development environment directly on Fedora,
    which comes at the cost of having to maintain both the Fedora-specific
    implementation and the Arch implementation, it should be easier to
    simply use Vagrant instead.

--- .fedora/vendor/crystal-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cat > /etc/yum.repos.d/crystal.repo <<END
-[crystal]
-name = Crystal
-baseurl = http://dist.crystal-lang.org/rpm/
-END
-
-rpm --import http://dist.crystal-lang.org/rpm/RPM-GPG-KEY
