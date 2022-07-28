d2eb6b702de451ead8e5c2a34649022dd14815b6
Donovan Glover <donovan@dglover.co>
2022-07-28 03:39:37 -0400

    gnome: Remove extensions
    This was cute, but it's easier to maintain GNOME by simply using stock
    instead of using a bunch of extensions.

--- gnome/.config/gnome/extensions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Use the Alternate Tab and Launch New Instance extensions
-gsettings set org.gnome.shell enabled-extensions "[
-  'alternate-tab@gnome-shell-extensions.gcampax.github.com',
-  'launch-new-instance@gnome-shell-extensions.gcampax.github.com'
-]"
