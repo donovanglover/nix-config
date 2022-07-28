b81a91e39fd62630badda4cb41b7e3c4e9474858
Donovan Glover <donovan@dglover.co>
2022-07-28 03:43:14 -0400

    meta: Remove gnome
    Having gnome settings was cute, but realistically the desktop
    environment should automatically choose the best settings for the target
    computer. Manually change anti-aliasing goes against the idea of wanting
    the stock GNOME experience.

--- gnome/.config/gnome/antialiasing.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Use the 'best' antialiasing settings for modern displays
-gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing rgba
-gsettings set org.gnome.settings-daemon.plugins.xsettings hinting full
