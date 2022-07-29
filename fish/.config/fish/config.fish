6aae63d66fb9b7d5907ffa88d938bda3e6c924cc
Donovan Glover <donovan@dglover.co>
2022-07-29 13:26:52 -0400

    fish: Add shift_jis conversion support
    A lot of files from Japan are encoded in the shift_jis character
    encoding. This abbreviation makes it easy to change those files to utf8.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -80,6 +80,7 @@ if not set -q set_abbr
     abbr cf     "tput reset"                # Clear the terminal completely
     abbr nf     "tput reset; and neofetch --size 56%; and xdotool key --delay 100 Ctrl+Shift+Page_Up"
     abbr df     "df --human-readable --type=ext4 --total"
+    abbr jis    "recode shift_jis..utf8"    # Easily convert shift_jis-encoded files to utf8
 
     abbr b      "feh --bg-fill"             # Change the background
     abbr c      "clear"                     # Because 5 letters is too much
