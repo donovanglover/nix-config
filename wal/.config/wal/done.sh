60f535331824cf7895d92ff0ab7de9670af63ba7
Donovan Glover <donovan@dglover.co>
2022-12-26 22:27:13 -0500

    wal: Remove bspwm/dunst/tint2 scripts
    By prioritizing GNOME, we're able to focus on being productive instead
    of customizing our linux setup.

--- wal/.config/wal/done.sh
+++ wal/.config/wal/done.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 
-if [ -e ~/.config/bspwm/wal.sh ]; then
-  ~/.config/bspwm/wal.sh
-fi
-
 if [ -e ~/.cache/wal/kitty ]; then
   for kitty_window in `fd kitty /tmp`; do
     kitty @ --to unix:$kitty_window set-colors -c -a ~/.cache/wal/kitty
   done
-fi
-
-if [ -e ~/.config/dunst/wal.sh ]; then
-  ~/.config/dunst/wal.sh
-fi
-
-if [ -e ~/.config/tint2/wal.sh ]; then
-  ~/.config/tint2/wal.sh
 fi
