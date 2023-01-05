7af4af4f491099815f7c4cc367e0f50eeaf79f71
Donovan Glover <donovan@dglover.co>
2023-01-05 02:16:30 -0500

    sxhkd: Add dropdown todos
    Since I now use GNOME, I'll never go through these TODOs, however
    anyone is free to implement these ideas on their own time.

--- sxhkd/.config/sxhkd/dropdown.sh
+++ sxhkd/.config/sxhkd/dropdown.sh
@@ -2,6 +2,8 @@
 # New Start: A modern Arch workflow built with an emphasis on functionality.
 # Copyright (C) 2018 Donovan Glover
 
+# TODO: Run this script when launching stuff, so we can use super+\ to launch anything + hide it again, and have terminal access
+
 DROPDOWN_DPI=$(xrdb -query | grep Xft.dpi | cut -f 2)
 DROPDOWN_SCALE=$(($DROPDOWN_DPI / 96))
 
@@ -15,6 +17,10 @@ x='kitty --name'  # Executable
 id=$(xdo id -n $i)
 rectangle=$((1700 * $DROPDOWN_SCALE))x$((600 * $DROPDOWN_SCALE))
 offset=+$((110 * $DROPDOWN_SCALE))+$((100 * $DROPDOWN_SCALE))
+# TODO: Use small window when holding shift
+# TODO: Just use fullscreen from native bspwm/xorg instead of manually setting size
+# rectangle=$((1920 * $DROPDOWN_SCALE))x$((1080 * $DROPDOWN_SCALE))
+# offset=+$((0 * $DROPDOWN_SCALE))+$((0 * $DROPDOWN_SCALE))
 
 bspc rule -r $c:$i
 bspc rule -a $c:$i sticky=on state=floating rectangle=$rectangle$offset
