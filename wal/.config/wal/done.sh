7bbd7281e589878a423cb84de7c89dcb4531ebc2
Donovan Glover <donovan@dglover.co>
2023-04-24 01:09:10 -0400

    wal: Simplify done.sh
    Since I plan to not use tint2 and bspwm anymore, there is no need for
    those scripts to run with done.sh.

--- wal/.config/wal/done.sh
+++ wal/.config/wal/done.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ -e ~/.config/bspwm/wal.sh ]; then
-  ~/.config/bspwm/wal.sh
-fi
-
 if [ -e ~/.cache/wal/kitty ]; then
   for kitty_window in `fd kitty /tmp`; do
     kitty @ --to unix:$kitty_window set-colors -c -a ~/.cache/wal/kitty
@@ -14,6 +10,4 @@ if [ -e ~/.config/dunst/wal.sh ]; then
   ~/.config/dunst/wal.sh
 fi
 
-if [ -e ~/.config/tint2/wal.sh ]; then
-  ~/.config/tint2/wal.sh
-fi
+notify-send "pywal" "Updated theme."
