dd027edc610f72baddb8cc1a9b7072d061791f82
Donovan Glover <donovan@dglover.co>
2023-04-24 01:07:53 -0400

    tint2: Call fullscreen.sh with wal.sh

--- tint2/.config/tint2/wal.sh
+++ tint2/.config/tint2/wal.sh
@@ -5,3 +5,6 @@ ln -sf ~/.cache/wal/tint2rc ~/.config/tint2/tint2rc
 
 # Restart tint2 with the new color scheme
 killall -SIGUSR1 tint2
+
+# Fix bar showing above fullscreen programs
+~/.config/tint2/fullscreen.sh
