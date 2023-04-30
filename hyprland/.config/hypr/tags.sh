68a8b3369cf3105811b9bb8020223f775b03662d
Donovan Glover <donovan@dglover.co>
2023-04-30 12:59:04 -0400

    hyprland: Add tags script
    This somewhat replicates the cool tag functionality observed in dwm,
    river, and similar window managers.

---
+++ hyprland/.config/hypr/tags.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Tag-like script that moves windows from X workspace to the current workspace
+
+# Get IDs of windows on the other workspace
+ids="$(hyprctl clients -j | jq -r ".[] | select(.workspace.id == "$1") | .address")"
+
+# Move those windows to the current workspace
+for id in $ids; do
+  hyprctl dispatch movetoworkspacesilent "$(hyprctl monitors | rg active | choose 2)",address:"$id"
+  hyprctl dispatch focuswindow address:"$id"
+done
