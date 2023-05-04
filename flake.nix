00fc33cad8d99f3b3105cd10d351cf7511fda141
Donovan Glover <donovan@dglover.co>
2023-05-04 13:09:45 -0400

    nix: Add flake.nix
    This may or may not work; I haven't tested it yet.

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/unstable";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs: {
+    nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.home-manager
+        { home-manager.useGlobalPkgs = true; }
+      ];
+    };
+  };
+}
