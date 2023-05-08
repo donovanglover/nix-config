c15dc299443e6634cb86453f4cd14510eb3b1d26
Donovan Glover <donovan@dglover.co>
2023-05-08 13:22:53 -0400

    meta: Remove bspwm/sxhkd
    I used bspwm for over half a decade, and although it was great, I am now
    interested in using Hyprland, which is basically bspwm for Wayland but
    better.

--- sxhkd/.config/sxhkd/selection.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-DATETIME=`date +%F_%H%M%S`
-
-if [[ "$1" == "clipboard" ]]; then
-  shotgun $(hacksaw -f "-i %i -g %g") - | xclip -t 'image/png' -selection clipboard
-else
-  shotgun $(hacksaw -f "-i %i -g %g") ~/$DATETIME.png
-fi
