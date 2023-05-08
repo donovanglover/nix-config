d654c01f30446eda7c598b6a12797f36d633fb09
Donovan Glover <donovan@dglover.co>
2023-05-08 13:19:45 -0400

    nix: Fix nixpkgs url

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
