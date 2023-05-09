b95cc4184ab5382c666800563ad427cea514ae88
Donovan Glover <donovan@dglover.co>
2023-05-09 14:08:27 -0400

    nix: Update editor.nix
    Was trying out some neovim plugins with Nix. Pretty cool that I don't
    have to worry about using *another* package manager like packer.nvim.

--- modules/editor.nix
+++ modules/editor.nix
@@ -10,7 +10,6 @@
           config = ''
             vim.g.loaded_netrw = 1
             vim.g.loaded_netrwPlugin = 1
-            vim.opt.termguicolors = true
             require("nvim-tree").setup()
           '';
         }
@@ -18,23 +17,71 @@
           plugin = indent-blankline-nvim;
           type = "lua";
           config = ''
-            vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
-            vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
-            require("indent_blankline").setup {
-              char = "",
-              char_highlight_list = {
-                "IndentBlanklineIndent1",
-                "IndentBlanklineIndent2",
-              },
-              space_char_highlight_list = {
-                "IndentBlanklineIndent1",
-                "IndentBlanklineIndent2",
-              },
-              show_trailing_blankline_indent = false,
-            }
+            require("indent_blankline").setup()
           '';
         }
+        {
+          plugin = barbar-nvim;
+          type = "lua";
+        }
+        {
+          plugin = gitsigns-nvim;
+          type = "lua";
+          config = ''
+            require('gitsigns').setup()
+          '';
+        }
+        {
+          plugin = nvim-web-devicons;
+          type = "lua";
+        }
+        {
+          plugin = autoclose-nvim;
+          type = "lua";
+          config = ''require("autoclose").setup()'';
+        }
+        {
+          plugin = nvim-scrollbar;
+          type = "lua";
+          config = ''require("scrollbar").setup()'';
+        }
+        {
+          plugin = nvim-base16;
+          type = "lua";
+          config = "vim.cmd('colorscheme base16-gruvbox-dark-soft')";
+        }
+        {
+          plugin = lualine-nvim;
+          type = "lua";
+          config = "require('lualine').setup()";
+        }
       ];
+    };
+    editorconfig = {
+      enable = true;
+      settings = {
+        "*" = {
+          charset = "utf-8";
+          end_of_line = "lf";
+          insert_final_newline = true;
+          indent_size = 2;
+          indent_style = "space";
+          trim_trailing_whitespace = true;
+        };
+        "*.md" = { indent_style = "tab"; };
+        "Makefile" = {
+          indent_style = "tab";
+          indent_size = 4;
+        };
+        "*.html" = {
+          indent_style = "tab";
+          indent_size = 4;
+        };
+        "*.go" = {
+          indent_style = "tab";
+          indent_size = 4;
+        };
+      };
     };
   };
 }
