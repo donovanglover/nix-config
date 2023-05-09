0230c9b4b0a0e0124b8677d99d3b482640c7867a
Donovan Glover <donovan@dglover.co>
2023-05-08 22:16:27 -0400

    home-manager: Use master branch

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
