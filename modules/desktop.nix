7dcaa2378e7cb7581e6f2016e1edbd9d9f2e312a
Donovan Glover <donovan@dglover.co>
2023-05-10 12:50:13 -0400

    fix: Migrate remaining kitty config to nix

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,18 +3,26 @@
   home-manager.users.user = { pkgs, ... }: {
     programs.kitty = {
       enable = true;
-      font = {
-        package = pkgs."maple-mono-NF";
-        name = "MapleMono-NF";
-      };
       settings = {
         enable_audio_bell = false;
         allow_remote_control = true;
         dynamic_background_opacity = true;
-        background_opacity = "0.9";
+        background_opacity = "0.95";
         close_on_child_death = true;
+        cursor_blink_interval = 0;
+        wayland_titlebar_color = "background";
+        listen_on = "unix:/tmp/kitty";
+        open_url_with = "librewolf";
+        window_padding_width = 10;
+        tab_bar_margin_width = 10;
       };
     };
+    xdg.configFile."kitty/diff.conf".text = ''
+      map d scroll_to next-page
+      map u scroll_to prev-page
+      map g scroll_to start
+      map G scroll_to end
+    '';
     programs.swaylock = {
       package = pkgs."swaylock-effects";
       settings = {
