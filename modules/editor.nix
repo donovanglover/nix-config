e05914ae1867527555999c571b7040f8c1f7d990
Donovan Glover <donovan@dglover.co>
2023-05-10 13:49:40 -0400

    feat: Add tig config

--- modules/editor.nix
+++ modules/editor.nix
@@ -127,5 +127,10 @@
         };
       };
     };
+    xdg.configFile."tig/config".text = ''
+      color cursor black green bold
+      color title-focus black blue bold
+      color title-blur black blue bold
+    '';
   };
 }
