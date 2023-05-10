cd32ad2e2717597391f7df68eb08631f48d2cac7
Donovan Glover <donovan@dglover.co>
2023-05-10 13:51:05 -0400

    feat: Add gtk config

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -17,6 +17,25 @@
       TerminalEmulator=kitty
       TerminalEmulatorDismissed=true
     '';
+    gtk = {
+      enable = true;
+      cursorTheme = {
+        package = pkgs.phinger-cursors;
+        name = "phinger-cursors";
+      };
+      gtk3.extraConfig = {
+        gtk-decoration-layout = "menu:";
+        gtk-xft-antialias = 1;
+        gtk-xft-hinting = 1;
+        gtk-xft-hintstyle = "hintfull";
+        gtk-xft-rgba = "rgb";
+        gtk-recent-files-enabled = false;
+      };
+      iconTheme = {
+        package = pkgs.fluent-icon-theme;
+        name = "Fluent";
+      };
+    };
     programs.kitty = {
       enable = true;
       settings = {
