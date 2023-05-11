7f4eac544652c5d1e87b3e5de13c7f0417fe306b
Donovan Glover <donovan@dglover.co>
2023-05-11 16:29:37 -0400

    Migrate remaining neovim config to nix

--- modules/editor.nix
+++ modules/editor.nix
@@ -4,12 +4,24 @@
     programs.neovim = {
       enable = true;
       extraConfig = ''
+        filetype plugin indent on
         set undofile
         set spell
         set number
         set clipboard=unnamedplus
+        set fileencoding=utf-8         " Ensure that we always save files as utf-8
+        set fileencodings=utf-8,sjis   " Automatically open shiftjis files with their proper encoding
+        set spelllang=en_us,cjk        " Don't show errors for CJK characters
+        set noshowmode                 " Disable the --MODE-- text (enable if not using the status line)
+        set mouse=a
+        set ignorecase                 " By default use case-insensitive search (combine with smartcase)
+        set smartcase                  " Make search case-sensitive when using capital letters
+        set scrolloff=1                " The minimal number of rows to show when scrolling up/down
+        set sidescrolloff=5            " The minimal number of columns to show when scrolling left/right
+        set tabstop=4                  " Show a tab character as 4 spaces
+        set softtabstop=0              " Edit soft tabs as if they're regular spaces
+        set shiftwidth=4               " Make autoindent appear as 4 spaces
 
-        autocmd VimLeave * set guicursor=a:block-blinkon1
 
         map <MiddleMouse> <Nop>
         imap <MiddleMouse> <Nop>
@@ -19,6 +31,27 @@
         imap <3-MiddleMouse> <Nop>
         map <4-MiddleMouse> <Nop>
         imap <4-MiddleMouse> <Nop>
+
+        highlight Search ctermbg=240 ctermfg=255
+        highlight IncSearch ctermbg=255 ctermfg=240
+
+        let mapleader = ' '
+        nnoremap <silent> <leader>e :set nu!<CR>
+        nnoremap <silent> <leader>t :OverCommandLine<CR>%s/
+        nnoremap <silent> <leader>a :Vexplore<CR>
+        nnoremap <silent> <leader>s :Sexplore<CR>
+        nnoremap <silent> <leader>d :Explore<CR>
+        nnoremap <silent> <leader>f :Files<CR>
+        nnoremap <silent> <leader>g :set hlsearch!<CR>
+        nnoremap <silent> <leader>j :Buffers<CR>
+        nnoremap <silent> <leader>k :NERDTreeToggleVCS<CR>
+        nnoremap <silent> <leader>l :Rg<CR>
+        nnoremap <silent> <leader>; <C-w>w
+        vnoremap <C-s> y:silent !notify-send -t 4000 "成果" "$(tango '<C-r>0')"<CR>:<Esc>
+
+        autocmd BufNewFile,BufRead *.ecr    setlocal syntax=html
+        autocmd BufWritePre,FileWritePre * silent! call mkdir(expand('<afile>:p:h'), 'p')
+
       '';
       plugins = with pkgs.vimPlugins; [
         {
@@ -58,11 +91,6 @@
         {
           plugin = nvim-web-devicons;
           type = "lua";
-        }
-        {
-          plugin = autoclose-nvim;
-          type = "lua";
-          config = ''require("autoclose").setup()'';
         }
         {
           plugin = nvim-scrollbar;
@@ -112,6 +140,32 @@
           plugin = clipboard-image-nvim;
           type = "lua";
         }
+        {
+          plugin = vimtex;
+          config = ''
+            " Disable all keybinds so we can define our own
+            let g:vimtex_mappings_enabled = 0
+            let g:vimtex_imaps_enabled = 0
+            let g:vimtex_view_method = 'zathura'
+            let g:vimtex_compiler_latexmk = {'build_dir': '.tex'}
+
+            " Set the normal keybinds
+            nnoremap <localleader>f <plug>(vimtex-view)
+            nnoremap <localleader>g <plug>(vimtex-compile)
+            nnoremap <localleader>d <plug>(vimtex-env-delete)
+            nnoremap <localleader>c <plug>(vimtex-env-change)
+          '';
+        }
+        vim-caddyfile
+        vim-graphql
+        vim-pug
+        vim-prisma
+        vim-javascript
+        vim-jsx-pretty
+        vim-vue
+        vim-over
+        vim-endwise
+        rust-vim
       ];
     };
     editorconfig = {
