ac3769d21c3dc8beafbcf9d714dd231f2f665469
Donovan Glover <donovan@dglover.co>
2023-05-11 00:29:03 -0400

    nix: Nixify dunst

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -84,6 +84,24 @@
     };
     services.dunst = {
       enable = true;
+      settings = {
+        global = {
+          geometry = "1870x5-25+45";
+          width = 350;
+          separator_height = 5;
+          padding = 24;
+          horizontal_padding = 24;
+          frame_width = 3;
+          idle_threshold = 120;
+          alignment = "center";
+          word_wrap = "yes";
+          transparency = 5;
+          format = "<b>%s</b>: %b";
+          markup = "full";
+          min_icon_size = 128;
+          max_icon_size = 128;
+        };
+      };
     };
     programs.waybar = {
       enable = true;
