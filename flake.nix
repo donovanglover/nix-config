23ddf7de1a8f0f2326e04f7f0705748002c36516
Donovan Glover <donovan@dglover.co>
2023-05-11 00:05:11 -0400

    nix: Nixify mpv

--- flake.nix
+++ flake.nix
@@ -33,7 +33,33 @@
             home.homeDirectory = "/home/user";
             home.packages = [ pkgs.httpie ];
             home.stateVersion = "22.11";
-            programs.mpv.enable = true;
+            programs.mpv = {
+              enable = true;
+              config = {
+                screenshot-format = "png";
+                profile = "gpu-hq";
+                scale = "ewa_lanczossharp";
+                cscale = "ewa_lanczossharp";
+                video-sync = "display-resample";
+                interpolation = true;
+                tscale = "oversample";
+                sub-auto = "fuzzy";
+                sub-font = "Noto Sans CJK JP Medium";
+                sub-blur = 10;
+                sub-file-paths = "subs:subtitles:字幕";
+                fullscreen = "yes";
+                title = "\${filename} - mpv";
+                script-opts = "osc-title=\${filename},osc-boxalpha=150,osc-showfullscreen=no,osc-boxvideo=yes";
+                osc = "no";
+                osd-on-seek = "no";
+                osd-bar = "no";
+                osd-bar-w = 30;
+                osd-bar-h = "0.2";
+                osd-duration = 750;
+                really-quiet = "yes";
+              };
+              scripts = [ pkgs.mpvScripts.thumbnail ];
+            };
             programs.librewolf = {
               enable = true;
               settings = {
