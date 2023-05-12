4109ed7b3b563e2923053d63a57706c794b5a6db
Donovan Glover <donovan@dglover.co>
2023-05-12 00:47:32 -0400

    hyprland: Remove old scripts

--- hyprland/.config/hypr/toggle-layout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [[ "$(hyprctl -j getoption general:layout | jq -r '.str')" == "master" ]]; then
-  hyprctl keyword general:layout "dwindle"
-else
-  hyprctl keyword general:layout "master"
-fi
