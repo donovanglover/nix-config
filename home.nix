b518a28f467d755e1af9c01393ba1ef904d54fad
Donovan Glover <donovan@dglover.co>
2023-05-14 13:13:59 -0400

    waybar: Change workspace numbers
    Might make this configurable later.

--- home.nix
+++ home.nix
@@ -559,6 +559,20 @@
           };
           "wlr/workspaces" = {
             on-click = "activate";
+            sort-by-number = true;
+            format = "{icon}";
+            format-icons = {
+              "1" = "一";
+              "2" = "二";
+              "3" = "三";
+              "4" = "四";
+              "5" = "五";
+              "6" = "六";
+              "7" = "七";
+              "8" = "八";
+              "9" = "九";
+              "10" = "十";
+            };
           };
           wireplumber = {
             format = "{icon}";
