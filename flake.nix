afc98ce47e5a6239da7aaed92a9e2a77585d8627
Donovan Glover <donovan@dglover.co>
2023-05-17 12:48:09 -0400

    Remove configuration.nix
    Not needed anymore since we know how to use nix modules.

--- flake.nix
+++ flake.nix
@@ -26,11 +26,50 @@
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [
-        ./configuration.nix
         ./home-manager
         hyprland.nixosModules.default
         stylix.nixosModules.stylix
         home-manager.nixosModules.home-manager
+        ./laptop.nix
+        ./modules/starship.nix
+        ./modules/fish.nix
+        ./modules/fonts.nix
+        ./modules/stylix
+        ./modules/htop.nix
+        ./modules/dual-function-keys.nix
+        ./modules/tlp.nix
+        ./modules/osu
+        ./modules/srb2
+        ./modules/mullvad
+        ./modules/pipewire
+        ./modules/networking
+        ./modules/virtualization
+        ./modules/xserver
+        ./modules/systemd
+        ./modules/vnstat
+        ./modules/locale
+        ./modules/firejail
+        ./modules/timezone
+        ./modules/nix
+        ./modules/npm
+        ./modules/home-manager
+        ./modules/piper
+        ./modules/packages
+        {
+          boot.loader.systemd-boot.enable = true;
+          boot.loader.efi.canTouchEfiVariables = true;
+
+          environment.sessionVariables = {
+            EDITOR = "nvim";
+            VISUAL = "nvim";
+            GIT_DISCOVERY_ACROSS_FILESYSTEM = "1";
+            FZF_DEFAULT_OPTS = "--height 40% --reverse --border --color=16";
+            NODE_OPTIONS = "--max_old_space_size=16384";
+          };
+
+          environment.defaultPackages = [ ];
+          system.stateVersion = "22.11";
+        }
       ];
     };
   };
