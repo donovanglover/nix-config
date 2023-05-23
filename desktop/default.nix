187028c5521847ab325902748f955083d5458746
Donovan Glover <donovan@dglover.co>
2023-05-23 17:14:24 -0400

    meta: Autologin with greetd and tuigreet

--- desktop/default.nix
+++ desktop/default.nix
@@ -29,5 +29,21 @@
     lnch
     wev
     swww
+    greetd.tuigreet
   ];
+
+  services.greetd = {
+    enable = true;
+    restart = false;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+      initial_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland";
+        user = "user";
+      };
+    };
+  };
 }
