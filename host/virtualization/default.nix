53d7a10509973f02ea414b7054c9394acde3ca4c
Donovan Glover <donovan@dglover.co>
2023-05-24 10:54:43 -0400

    virtualization: Enable auto-login

--- host/virtualization/default.nix
+++ host/virtualization/default.nix
@@ -13,6 +13,11 @@
     services.xserver.desktopManager.gnome.enable = true;
     services.xserver.displayManager.gdm.enable = true;
 
+    services.xserver.displayManager.autoLogin.enable = true;
+    services.xserver.displayManager.autoLogin.user = "user";
+    systemd.services."getty@tty1".enable = false;
+    systemd.services."autovt@tty1".enable = false;
+
     i18n.inputMethod = lib.mkDefault {
       enabled = "ibus";
       ibus.engines = with pkgs.ibus-engines; [ mozc ];
