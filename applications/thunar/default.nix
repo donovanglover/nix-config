6af5e5c6cc1a0148cf7fa18e87b2e524d0d4c9ac
Donovan Glover <donovan@dglover.co>
2023-05-25 17:11:30 -0400

    meta: Enable gvfs
    This gives thunar, nvim-tree, and other programs access to the trash and
    other directories.

--- applications/thunar/default.nix
+++ applications/thunar/default.nix
@@ -2,8 +2,9 @@
 
 {
   programs.thunar.enable = true;
-  services.tumbler.enable = true;
-  environment.systemPackages = [ pkgs.xfce.exo ];
+  services.tumbler.enable = true; # Thumbnail support
+  environment.systemPackages = [ pkgs.xfce.exo ]; # Open with kitty support
+  services.gvfs.enable = true; # Trash support
 
   home-manager.sharedModules = [{
     xdg.configFile."xfce4/helpers.rc".text = ''
