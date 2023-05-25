93d9693f4c4f35d0f73a58081941bc24a3c3c516
Donovan Glover <donovan@dglover.co>
2023-05-25 00:35:41 -0400

    Merge all dev modules together
    Unlike applications which are usually independent of each other,
    development tools are often used as needed.

--- dev/default.nix
+++ dev/default.nix
@@ -1,12 +1,41 @@
 { pkgs, ... }:
 
 {
-  imports = [
-    ./editorconfig
-    ./nix
-    ./npm
-    ./rust
-  ];
+  programs.npm.enable = true;
+
+  home-manager.sharedModules = [{
+    editorconfig = {
+      enable = true;
+
+      settings = {
+        "*" = {
+          charset = "utf-8";
+          end_of_line = "lf";
+          insert_final_newline = true;
+          indent_size = 2;
+          indent_style = "space";
+          trim_trailing_whitespace = true;
+        };
+
+        "*.md".indent_style = "tab";
+
+        "Makefile" = {
+          indent_style = "tab";
+          indent_size = 4;
+        };
+
+        "*.html" = {
+          indent_style = "tab";
+          indent_size = 4;
+        };
+
+        "*.go" = {
+          indent_style = "tab";
+          indent_size = 4;
+        };
+      };
+    };
+  }];
 
   environment.systemPackages = with pkgs; [
     marksman
@@ -14,5 +43,23 @@
     lua-language-server
     clang-tools
     texlab
+
+    # nix
+    nil
+    nixfmt
+    nixos-generators
+
+    # node/yarn/deno
+    nodejs
+    yarn
+    deno
+
+    # rust
+    gcc
+    rustc
+    rustfmt
+    cargo
+    rust-analyzer
+    bacon
   ];
 }
--- dev/nix/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  environment.systemPackages = with pkgs; [
-    nil
-    nixfmt
-    nixos-generators
-  ];
-}
