3c2dbb4915127d03c6d8fe03ac8ee93cf8eece77
Donovan Glover <donovan@dglover.co>
2023-05-26 10:41:07 -0400

    Use a more sane default locale

--- common.nix
+++ common.nix
@@ -6,9 +6,9 @@
   ];
 
   # locale
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "ja_JP.UTF-8";
   i18n.supportedLocales =
-    [ "en_US.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" "fr_FR.UTF-8/UTF-8" ];
+    [ "ja_JP.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" "fr_FR.UTF-8/UTF-8" ];
 
   # nix
   nix = {
