f8f54d189c6e27584f92d3aebf93b06126940970
Donovan Glover <donovan@dglover.co>
2023-05-31 10:46:59 -0400

    joshuto: Increase preview size to 10 GB

--- terminal/joshuto/default.nix
+++ terminal/joshuto/default.nix
@@ -15,7 +15,7 @@
       collapse_preview = false
 
       [preview]
-      max_preview_size = 104857600
+      max_preview_size = 10000000000
       preview_script = "~/.config/joshuto/preview.sh"
       preview_shown_hook_script = "~/.config/joshuto/kitty-show.sh"
       preview_removed_hook_script = "~/.config/joshuto/kitty-remove.sh"
