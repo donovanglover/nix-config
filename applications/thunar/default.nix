3950731e821d1e219a621fe8ce5d09bcbe75a287
Donovan Glover <donovan@dglover.co>
2023-06-01 15:17:08 -0400

    thunar: Add gnome-keyring
    This prevents thunar from showing an error message when mounting an
    encrypted partition.

--- applications/thunar/default.nix
+++ applications/thunar/default.nix
@@ -5,6 +5,7 @@
   services.tumbler.enable = true; # Thumbnail support
   environment.systemPackages = [ pkgs.xfce.exo ]; # Open with kitty support
   services.gvfs.enable = true; # Trash support
+  services.gnome.gnome-keyring.enable = true; # Mount support
 
   home-manager.sharedModules = [{
     xdg.configFile."xfce4/helpers.rc".text = ''
