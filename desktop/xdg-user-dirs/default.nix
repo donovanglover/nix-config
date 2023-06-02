e6e4b56a89f922477bb5c3869a88313bd7c24dd8
Donovan Glover <donovan@dglover.co>
2023-06-02 17:39:51 -0400

    Use correct locale for xdg-user-dirs

--- desktop/xdg-user-dirs/default.nix
+++ desktop/xdg-user-dirs/default.nix
@@ -1,5 +1,19 @@
 {
   home-manager.sharedModules = [{
-    xdg = { userDirs = { enable = true; }; };
+    xdg.userDirs = {
+      enable = true;
+
+      desktop = null;
+      templates = null;
+      publicShare = null;
+
+      download = "/home/user/ダウンロード";
+      documents = "/home/user/ドキュメント";
+      music = "/home/user/音楽";
+      pictures = "/home/user/画像";
+      videos = "/home/user/ビデオ";
+    };
+
+    xdg.configFile."user-dirs.locale".text = "ja_JP";
   }];
 }
