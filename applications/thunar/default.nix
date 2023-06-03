5a7816aec814e72c09a30d57c7b29221bb124006
Donovan Glover <donovan@dglover.co>
2023-06-03 10:34:35 -0400

    thunar: Add glib
    Unfortunately I don't remember why glib is needed but it may have been
    mounting related.

--- applications/thunar/default.nix
+++ applications/thunar/default.nix
@@ -3,7 +3,11 @@
 {
   programs.thunar.enable = true;
   services.tumbler.enable = true; # Thumbnail support
-  environment.systemPackages = [ pkgs.xfce.exo ]; # Open with kitty support
+  environment.systemPackages = with pkgs; [
+    xfce.exo # Open with kitty support
+    glib
+    (pkgs.callPackage ./go-thumbnailer {})
+  ];
   services.gvfs.enable = true; # Trash support
   services.gnome.gnome-keyring.enable = true; # Mount support
 
