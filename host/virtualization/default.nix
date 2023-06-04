0bf134297b3a62da62f9ee16439d6da995d3fbff
Donovan Glover <donovan@dglover.co>
2023-06-04 11:42:45 -0400

    virtualization: Fix cursor in hyprland
    This commit fixes the cursor being upside down and inaccurate in
    Hyprland. Note that show-cursor=off is used to avoid the duplicate
    cursor issue described in https://github.com/swaywm/sway/issues/6581
    
    Unlike GNOME, Hyprland does not automatically change resolution on
    window change, so fullscreen is enabled by default to ensure a certain
    size. In the future, a script could be used to adjust the resolution as
    needed.
    
    Related: https://github.com/hyprwm/Hyprland/issues/1056

--- host/virtualization/default.nix
+++ host/virtualization/default.nix
@@ -6,6 +6,10 @@
     };
 
     virtualisation.qemu.options =
-      [ "-device virtio-vga-gl" "-display sdl,gl=on" ];
+      [ "-device virtio-vga-gl" "-display sdl,gl=on,show-cursor=off" "-full-screen" ];
+
+    environment.sessionVariables = {
+      WLR_NO_HARDWARE_CURSORS = "1";
+    };
   };
 }
