f5e0505f93b2c9658ebe393d4583440d8899aa5a
Donovan Glover <donovan@dglover.co>
2023-06-03 20:50:14 -0400

    Remove gnome scripts
    Although this was useful at some point to make GNOME usable, a virtual
    GNOME instance works quite fine without this script.
    
    Whether I even need GNOME virtualization is debatable due to how much I
    was able to achieve with containers. Benefits of containers include not
    having to start up a virtual machine, easy sharing of files with the
    host, and having the window manager manage all windows.

--- gnome/toggle.sh
+++
@@ -1,7 +0,0 @@
-status=$(gsettings get org.gnome.shell enabled-extensions)
-
-if [[ "$status" == *'hidetopbar@mathieu.bidon.ca'* ]]; then
-  gnome-extensions disable hidetopbar@mathieu.bidon.ca
-else
-  gnome-extensions enable hidetopbar@mathieu.bidon.ca
-fi
