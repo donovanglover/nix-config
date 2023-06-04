91be6afd3f90e72b439e7eac41285be7d0298895
Donovan Glover <donovan@dglover.co>
2023-06-04 11:40:28 -0400

    virtualization: Remove GNOME
    I originally used GNOME for virtualization because the cursor in
    Hyprland was upside down and its position was offset by a noticeable
    amount. However, now that I've figured out how to make Hyprland work
    under QEMU with an accurate cursor, this is no longer needed.

--- host/virtualization/default.nix
+++ host/virtualization/default.nix
@@ -1,5 +1,3 @@
-{ pkgs, lib, ... }:
-
 {
   virtualisation.vmVariant = {
     virtualisation = {
@@ -9,26 +7,5 @@
 
     virtualisation.qemu.options =
       [ "-device virtio-vga-gl" "-display sdl,gl=on" ];
-
-    services.xserver.desktopManager.gnome.enable = true;
-    services.xserver.displayManager.gdm.enable = true;
-
-    services.xserver.displayManager.autoLogin.enable = true;
-    services.xserver.displayManager.autoLogin.user = "user";
-    systemd.services."getty@tty1".enable = false;
-    systemd.services."autovt@tty1".enable = false;
-
-    i18n.inputMethod = lib.mkDefault {
-      enabled = "ibus";
-      ibus.engines = with pkgs.ibus-engines; [ mozc ];
-    };
-
-    services.gnome.core-utilities.enable = false;
-    environment.gnome.excludePackages = [ pkgs.gnome-tour ];
-
-    services.pipewire.enable = lib.mkForce false;
-    services.mullvad-vpn.enable = lib.mkForce false;
-    programs.hyprland.enable = lib.mkForce false;
-    services.greetd.enable = lib.mkForce false;
   };
 }
