30ccf622f7003fd5114c52687aaf1e7c48be3092
Donovan Glover <donovan@dglover.co>
2023-06-05 12:12:12 -0400

    nix: Merge user module

--- common.nix
+++ common.nix
@@ -3,7 +3,6 @@
 {
   imports = [
     ./modules
-    ./user.nix
     ./desktop
     ./dev
     ./host
@@ -86,4 +85,21 @@
 
   # timezone
   time.timeZone = "America/New_York";
+
+  # user
+  users = {
+    mutableUsers = false;
+
+    users.user = {
+      isNormalUser = true;
+      uid = 1000;
+      password = "user";
+      extraGroups = [ "wheel" "networkmanager" ];
+    };
+  };
+
+  home-manager.users.user = {
+    home.username = "user";
+    home.homeDirectory = "/home/user";
+  };
 }
