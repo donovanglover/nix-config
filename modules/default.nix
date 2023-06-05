343076f94276a5d057be3edd565d676e7e530179
Donovan Glover <donovan@dglover.co>
2023-06-05 11:39:55 -0400

    meta: Move terminal files to modules
    Since all of these files do roughly the same thing (that is, configure
    the system in a specific way that a separate file seems necessary), this
    should reduce the overall complexity of the project tree.

--- modules/default.nix
+++ modules/default.nix
@@ -10,6 +10,13 @@
     ./qutebrowser
     ./thunar
     ./zathura
+
+    ./fish-starship
+    ./git
+    ./gpg
+    ./ncmpcpp
+    ./neovim
+    ./joshuto
   ];
 
   virtualisation.vmware.host = {
@@ -30,5 +37,83 @@
     spek
     keepassxc
     libreoffice
+
+    wget
+    jq
+    exa
+    fd
+    fzf
+    gdu
+    fdupes
+    mediainfo
+    ponysay
+    lolcat
+    cmatrix
+    sox
+    httpie
+    p7zip
+    ripgrep
+    rsync
+    unar
+    genact
+    ffmpeg
+    killall
+    trashy
+    whois
+    dwt1-shell-color-scripts
+    dig
+    yt-dlp
+    neofetch
+    pywal
+    brightnessctl
+    zellij
+    librespeed-cli
+    wiki-tui
+    hexyl
+    nb
+    jpegoptim
+    playerctl
+    recode
+    rmlint
+    sd
+    crystal
+    shards
+    smartmontools
+    sqlitebrowser
+    visidata
+    scc
+    hwinfo
+    stress
+    choose
+    gum
+    hdparm
+    imagemagick
+    onefetch
+    restic
+    wails
+    watchexec
+    memento
+    mpvpaper
+    timg
+    kanjidraw
+    ventoy
+    wf-recorder
+    mdcat
+    mdbook
+    zola
+    file
+    tessen
+    wtype
+    mtr
   ];
+
+  home-manager.sharedModules = [{
+    programs.bat.enable = true;
+  }];
+
+  programs.htop = {
+    enable = true;
+    package = pkgs."htop-vim";
+    settings = { tree_view = 1; };
+  };
 }
