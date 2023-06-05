46c1c99ab64ab9126c555083638a62a276bdb8eb
Donovan Glover <donovan@dglover.co>
2023-06-05 18:54:33 -0400

    nix: Simplify nix options
    Writing nix twice here actually makes things easier to read, and should
    be preferred over expansion which results in more lines of code.

--- common.nix
+++ common.nix
@@ -13,12 +13,8 @@
     [ "ja_JP.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" "fr_FR.UTF-8/UTF-8" ];
 
   # nix
-  nix = {
-    package = pkgs.nixFlakes;
-    settings = {
-      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
-    };
-  };
+  nix.package = pkgs.nixFlakes;
+  nix.settings.experimental-features = [ "nix-command" "flakes" "repl-flake" ];
 
   hardware.opengl.driSupport32Bit = true;
 
