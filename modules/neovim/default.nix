f0867c039e6c108037ea55601ceff77212df7654
Donovan Glover <donovan@dglover.co>
2023-06-06 07:27:04 -0400

    neovim: Open tree by default

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -69,6 +69,19 @@
                   end
                 end
               })
+
+              local function open_nvim_tree(data)
+                local real_file = vim.fn.filereadable(data.file) == 1
+                local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+                if not real_file and not no_name then
+                  return
+                end
+
+                require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+              end
+
+              vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
             '';
           }
           {
