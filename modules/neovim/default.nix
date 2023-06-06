486178b27fa8c4578b1fa70c6134078dc6276a29
Donovan Glover <donovan@dglover.co>
2023-06-06 10:52:39 -0400

    Revert "neovim: Focus tree when empty buffer"
    This actually causes QuitPre to not close neovim since the tree is
    closed first when quitting while the tree is focused. For simplicity,
    the tree should always be unfocused to avoid ambiguity.

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -73,7 +73,7 @@
                   return
                 end
 
-                require("nvim-tree.api").tree.toggle({ focus = no_name, find_file = true })
+                require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
               end
 
               vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
