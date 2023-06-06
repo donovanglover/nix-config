c59387bb1fe261a51a992e243a77dceeda5bcc61
Donovan Glover <donovan@dglover.co>
2023-06-06 07:50:36 -0400

    neovim: Close tree with QuitPre
    This fixes an issue where neovim would automatically close when opening
    it with a directory.

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -61,13 +61,8 @@
             config = ''
               require("nvim-tree").setup()
 
-              vim.api.nvim_create_autocmd("BufEnter", {
-                nested = true,
-                callback = function()
-                  if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-                    vim.cmd "quit"
-                  end
-                end
+              vim.api.nvim_create_autocmd({"QuitPre"}, {
+                  callback = function() vim.cmd("NvimTreeClose") end
               })
 
               local function open_nvim_tree(data)
