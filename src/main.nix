d4f0a45da39f7da70cf5f0a9442378b0f8d2fa7a
Donovan Glover <donovan@dglover.co>
2023-06-07 12:26:12 -0400

    gamemode: Customize

--- src/main.nix
+++ src/main.nix
@@ -39,7 +39,20 @@ in {
 
   boot.tmp.useTmpfs = true;
 
-  programs.gamemode.enable = true;
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        renice = 10;
+        igpu_power_threshold = -1;
+      };
+
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'Note' 'gamemode started.'";
+        end = "${pkgs.libnotify}/bin/notify-send 'Note' 'gamemode ended.";
+      };
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     hypr-contrib.packages."${VARIABLES.system}".grimblast
