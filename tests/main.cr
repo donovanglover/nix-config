cfcb2033a871cdaad13f9dbdf997226789f64fe8
Donovan Glover <donovan@dglover.co>
2023-06-08 19:11:40 -0400

    tests: Ensure latest joshuto commit

--- tests/main.cr
+++ tests/main.cr
@@ -1,5 +1,7 @@
 require "spec"
 require "colorize"
+require "http/client"
+require "json"
 
 describe "nix-config" do
   it "includes all modules" do
@@ -11,6 +13,19 @@ describe "nix-config" do
       print "Checking ./modules/#{current_module}...".colorize(:blue)
       modules.includes?("./#{current_module}").should be_true
       puts "✓".colorize(:green)
+    end
+  end
+
+  it "uses the latest joshuto commit" do
+    response = HTTP::Client.get "https://api.github.com/repos/kamiyaa/joshuto/branches/main"
+    response.status_code.should eq(200)
+    json = JSON.parse(response.body)
+
+    File.read_lines("./overlays/joshuto/default.nix").each do |line|
+      if line.includes? "version ="
+        nix_hash = line.split('"')[1]
+        json["commit"]["sha"].should eq(nix_hash)
+      end
     end
   end
 end
