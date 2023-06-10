ad377d14fa1bd02b281cce9a2f18ef71e2e9f953
Donovan Glover <donovan@dglover.co>
2023-06-10 08:16:48 -0400

    overlays: Add kitty duplicate lines patch
    Patches the issue described below:
    https://github.com/kovidgoyal/kitty/issues/6306

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./alejandra
     ./joshuto
+    ./kitty
     ./rofi
   ];
 }
---
+++ overlays/kitty/default.nix
@@ -0,0 +1,9 @@
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      kitty = prev.kitty.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [./fix-duplicate-lines.patch];
+      });
+    })
+  ];
+}
