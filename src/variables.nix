a2886940f48f84f90278635610e7118247584f5b
Donovan Glover <donovan@dglover.co>
2023-06-12 13:26:36 -0400

    nix: Move hosts to src
    Might change this later once home-manager and nixos modules are
    separated.

--- src/variables.nix
+++ src/variables.nix
@@ -2,7 +2,7 @@
   hostname = "nixos";
   system = "x86_64-linux";
   timezone = "America/New_York";
-  hostHardwareConfiguration = ../hosts/laptop.nix;
+  hostHardwareConfiguration = ./laptop.nix;
   stateVersion = "22.11";
   defaultLocale = "ja_JP.UTF-8";
   supportedLocales = [ "ja_JP.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" "fr_FR.UTF-8/UTF-8" ];
