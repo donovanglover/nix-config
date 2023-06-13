8ce1e73531e0e195f4e22555aa993055dddbaac6
Donovan Glover <donovan@dglover.co>
2023-06-12 20:50:42 -0400

    containers: Add srb2 container
    Previously I decided against using srb2 in a container due to the poor
    performance I experienced. Since I figured out how to use the graphics
    from the host inside of the container, performance is no longer an
    issue.

---
+++ containers/srb2.nix
@@ -0,0 +1,65 @@
+{ home-manager, stylix, nixpkgs-srb2, ... }:
+let
+  VARIABLES = import ../src/variables.nix;
+in
+{
+  containers.srb2 = {
+    privateNetwork = true;
+    ephemeral = true;
+    autoStart = true;
+
+    bindMounts = {
+      "/home/user/.srb2" = {
+        hostPath = "/home/${VARIABLES.username}/containers/srb2";
+        isReadOnly = false;
+      };
+
+      waylandDisplay = rec {
+        hostPath = "/run/user/1000";
+        mountPoint = hostPath;
+      };
+
+      x11Display = rec {
+        hostPath = "/tmp/.X11-unix";
+        mountPoint = hostPath;
+        isReadOnly = true;
+      };
+    };
+
+    config = { pkgs, lib, ... }: {
+      imports = [
+        home-manager.nixosModules.home-manager
+        stylix.nixosModules.stylix
+        ./common/wayland.nix
+        ../modules/fonts
+        ../modules/stylix
+      ];
+
+      environment.systemPackages = with pkgs; [
+        nixpkgs-srb2.legacyPackages.${VARIABLES.system}.srb2
+        kitty
+      ];
+
+      users = {
+        mutableUsers = false;
+        allowNoPasswordLogin = true;
+
+        users.user = {
+          isNormalUser = true;
+          home = "/home/user";
+        };
+      };
+
+      home-manager.users.user = { ... }: {
+        home.stateVersion = VARIABLES.stateVersion;
+      };
+
+      environment = {
+        variables = { TERM = "xterm-kitty"; };
+        defaultPackages = [ ];
+      };
+
+      system.stateVersion = VARIABLES.stateVersion;
+    };
+  };
+}
