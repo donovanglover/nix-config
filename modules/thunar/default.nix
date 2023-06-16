b12e1598c9efce1e19fa5ce5f480ddce6d9857f5
Donovan Glover <donovan@dglover.co>
2023-06-16 13:19:38 -0400

    thunar: Add volman
    This should prevent errors from popping up in the terminal when mounting
    disks.

--- modules/thunar/default.nix
+++ modules/thunar/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   programs.thunar.enable = true;
   services.tumbler.enable = true; # Thumbnail support
+  programs.thunar.plugins = with pkgs.xfce; [ thunar-volman ];
 
   environment.systemPackages = with pkgs; [
     xfce.exo # Open with kitty support
