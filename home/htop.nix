ebd96d805eceda940f366dc6383ad8a3a3f299cc
Donovan Glover <donovan@dglover.co>
2023-06-17 10:48:18 -0400

    htop: Use better settings for NixOS
    Although many people have tried to make more modern top-like programs,
    my favorite is still htop *by far*. NixOS includes htop-vim in its
    official repositories, which is great, and this change removes the
    /nix/store prefixes from all the processes, making htop overall much
    easier to read and navigate.

--- home/htop.nix
+++ home/htop.nix
@@ -4,6 +4,14 @@
   programs.htop = {
     enable = true;
     package = pkgs."htop-vim";
-    settings = { tree_view = 1; };
+    settings = {
+      tree_view = 1;
+      hide_userland_threads = 1;
+      highlight_changes = 1;
+      show_cpu_frequency = 1;
+      show_cpu_temperature = 1;
+      show_program_path = 0;
+      highlight_base_name = 1;
+    };
   };
 }
