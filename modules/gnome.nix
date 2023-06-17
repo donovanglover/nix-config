0e6ed0aa9310339e4abeda1bf085949fbd38299f
Donovan Glover <donovan@dglover.co>
2023-06-17 09:18:24 -0400

    meta: Add back gnome module
    Possibly useful for setting up computers with GNOME. The main advantage
    GNOME has is the ability to have a consistent environment in both X11
    and Wayland, which is useful to test whether or not something only works
    in X11.

---
+++ modules/gnome.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    displayManager.lightdm.enable = false;
+    excludePackages = [ pkgs.xterm ];
+  };
+
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "user";
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  i18n.inputMethod = {
+    enabled = "ibus";
+    ibus.engines = with pkgs.ibus-engines; [ mozc ];
+ };
+
+  services.gnome.core-utilities.enable = false;
+  environment.gnome.excludePackages = [ pkgs.gnome-tour ];
+
+  hardware.pulseaudio.enable = false;
+}
