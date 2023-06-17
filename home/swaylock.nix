ef109a94720f109f1afd3449d648b37026260349
Donovan Glover <donovan@dglover.co>
2023-06-16 20:16:19 -0400

    swaylock: Move to ./home

---
+++ home/swaylock.nix
@@ -0,0 +1,17 @@
+{
+  programs.swaylock = {
+    package = pkgs."swaylock-effects";
+    settings = {
+      show-keyboard-layout = true;
+      daemonize = true;
+      effect-blur = "20x2";
+      clock = true;
+      indicator = true;
+      font-size = 25;
+      indicator-radius = 85;
+      indicator-thickness = 16;
+      screenshots = true;
+      fade-in = 1;
+    };
+  };
+}
