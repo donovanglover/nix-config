c0966633d82e8921b65c94c3da9ee550a3ad2d57
Donovan Glover <donovan@dglover.co>
2023-06-16 20:15:45 -0400

    librewolf: Move to ./home

---
+++ home/librewolf.nix
@@ -0,0 +1,20 @@
+{
+  programs.librewolf = {
+    enable = true;
+
+    settings = {
+      "middlemouse.paste" = false;
+
+      "ui.use_activity_cursor" = true;
+
+      "privacy.resistFingerprinting.letterboxing" = true;
+
+      "browser.download.useDownloadDir" = true;
+      "browser.tabs.insertAfterCurrent" = true;
+      "browser.toolbars.bookmarks.visibility" = "never";
+
+      "sidebar.position_start" = false;
+      "findbar.highlightAll" = true;
+    };
+  };
+}
