da70c0293bedf6716f55a82c908184241913cb1f
Donovan Glover <donovan@dglover.co>
2023-06-20 14:50:03 -0400

    overlays(waybar): Use latest commit
    Adds support for https://github.com/Alexays/Waybar/issues/2211

--- overlays/waybar.nix
+++ overlays/waybar.nix
@@ -1,7 +1,16 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      waybar = prev.waybar.overrideAttrs (old: {
+      waybar = prev.waybar.overrideAttrs (old: rec {
+        version = "b0f89f2bc115f4447dbf4565faca9a6122594d68";
+
+        src = final.fetchFromGitHub {
+          owner = "Alexays";
+          repo = "Waybar";
+          rev = version;
+          sha256 = "sha256-wH8UvSYnEBCdF2r6yAtocVuaZM9eIXLlg31n9FKTISE=";
+        };
+
         postPatch = /* bash */ ''
           # use hyprctl to switch workspaces
           sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace e+1";\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
