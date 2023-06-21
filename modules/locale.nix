43186c630e9b8b95ce92686f36580d6bfef7666f
Donovan Glover <donovan@dglover.co>
2023-06-21 03:32:56 -0400

    locale: Fix import

--- modules/locale.nix
+++ modules/locale.nix
@@ -1,4 +1,4 @@
-let VARIABLES = import ./variables.nix; in {
+let VARIABLES = import ../src/variables.nix; in {
   i18n.defaultLocale = VARIABLES.defaultLocale;
   i18n.supportedLocales = VARIABLES.supportedLocales;
 }
