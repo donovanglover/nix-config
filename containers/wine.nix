a75c20db5bfb7340fbf570327911502fa3101b5a
Donovan Glover <donovan@dglover.co>
2023-06-21 11:27:02 -0400

    containers(wine): Switch to wineWowPackages.stagingFull
    wineWowPackages.stagingFull has better compatibility than waylandFull
    and runs more applications without black screens. The difference between
    stagingFull and wine-ge is that wine-ge doesn't crash when encountering
    an error like ELFCLASS64 and usually opens windows larger and
    fullscreen-like.
    
    The reason stagingFull is preferred over wine-ge in this case is that
    stagingFull is a part of nixpkgs and doesn't suffer from a black screen
    bug when a hidden menu pushes the content in the window viewport down.
    
    Additionally, although wine-ge avoids crashes in more cases, this
    results in applications running that may or may not fully work, and
    sometimes results in black screens where there should be graphics
    instead.

--- containers/wine.nix
+++ containers/wine.nix
@@ -71,7 +71,7 @@ let VARIABLES = import ../src/variables.nix; in {
 
       environment.systemPackages = with pkgs; [
         kitty
-        nix-gaming.packages."${VARIABLES.system}".wine-ge
+        wineWowPackages.stagingFull
         winetricks
       ];
 
