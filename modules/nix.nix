59eb54e1e503d59f8d71411d2d3fa8e1e3770d52
Donovan Glover <donovan@dglover.co>
2023-06-22 09:31:39 -0400

    nix: Remove nix-gaming cache
    No longer needed since I plan to just use the wine from nixpkgs.

--- modules/nix.nix
+++ modules/nix.nix
@@ -7,9 +7,6 @@
     settings = {
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       auto-optimise-store = true;
-
-      substituters = [ "https://nix-gaming.cachix.org" ];
-      trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
     };
   };
 }
