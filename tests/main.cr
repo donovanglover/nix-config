79f4ae0296c3a4297ef9f693cbfd7cd439dc1d8c
Donovan Glover <donovan@dglover.co>
2023-06-22 07:26:18 -0400

    tests: Add tests for containers and specializations

--- tests/main.cr
+++ tests/main.cr
@@ -2,9 +2,11 @@ require "spec"
 require "./check_top_level_imports"
 require "./check_latest_commit"
 
+check_top_level_imports("containers")
+check_top_level_imports("home")
 check_top_level_imports("modules")
 check_top_level_imports("overlays")
-check_top_level_imports("home")
+check_top_level_imports("specializations")
 
 describe "./overlays/joshuto/default.nix", tags: "online" do
   it "uses the latest joshuto commit" do
