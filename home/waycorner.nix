7209bae4a3d7abd0c91a5a4a8ea5334d51472a75
Donovan Glover <donovan@dglover.co>
2023-06-23 19:30:22 -0400

    waycorner: Add failed waybar attempt
    This was my attempt at using waycorner with waybar, however it fails
    since waybar shows above waycorner. This commit is purely for historical
    purposes.

--- home/waycorner.nix
+++ home/waycorner.nix
@@ -4,18 +4,11 @@
   home.packages = with pkgs; [ waycorner ];
 
   xdg.configFile."waycorner/config.toml".text = /* toml */ ''
-    [application_launcher]
-    enter_command = [ "hyprctl", "dispatch", "workspace", "empty" ]
-    exit_command = [ "${pkgs.lnch}/bin/lnch", "${pkgs.rofi}/bin/rofi", "-show", "drun" ]
-    locations = ["top_right"]  # default
-    size = 10
-    timeout_ms = 250
-
-    [show_desktop]
-    enter_command = [ "hyprctl", "dispatch", "workspace", "empty" ]
-    exit_command = [ "hyprctl", "dispatch", "workspace", "previous"]
-    locations = ["bottom_right"]  # default
-    size = 10
+    [waybar]
+    enter_command = [ "waybar" ]
+    exit_command = [ "killall", ".waybar-wrapped" ]
+    locations = ["right"]  # default
+    size = 40
     timeout_ms = 250
   '';
 }
