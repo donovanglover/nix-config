9dc1d0043d67a1f8b1758611215ac8dd0af47571
Donovan Glover <donovan@dglover.co>
2023-06-24 12:06:13 -0400

    containers(dev): Enable gui usage

--- containers/dev.nix
+++ containers/dev.nix
@@ -12,7 +12,30 @@
         hostPath = "/home/user/containers/dev";
         isReadOnly = false;
       };
+
+      waylandDisplay = rec {
+        hostPath = "/run/user/1000";
+        mountPoint = hostPath;
+      };
+
+      x11Display = rec {
+        hostPath = "/tmp/.X11-unix";
+        mountPoint = hostPath;
+        isReadOnly = true;
+      };
+
+      dri = rec {
+        hostPath = "/dev/dri";
+        mountPoint = hostPath;
+      };
     };
+
+    allowedDevices = [
+      {
+        modifier = "rw";
+        node = "/dev/dri/renderD128";
+      }
+    ];
 
     config = { pkgs, ... }: {
       imports = [
