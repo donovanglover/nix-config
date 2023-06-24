e677cebc44f0c7a212c6013be77698c9ade378c7
Donovan Glover <donovan@dglover.co>
2023-06-23 21:10:07 -0400

    overlays(waybar): Dispatch selected workspace

--- overlays/waybar.nix
+++ overlays/waybar.nix
@@ -13,7 +13,7 @@
 
         postPatch = /* bash */ ''
           # use hyprctl to switch workspaces
-          sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace e+1";\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
+          sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
         '';
 
         mesonFlags = old.mesonFlags ++ [ "-Dexperimental=true" ];
