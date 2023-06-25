84eb63b14ae67abdbd61c756ecc884ebc60b4710
Donovan Glover <donovan@dglover.co>
2023-06-25 12:24:57 -0400

    waybar: Simplify

--- home/waybar.nix
+++ home/waybar.nix
@@ -86,23 +86,23 @@
       };
     };
 
-    style = lib.mkForce /* css */ ''
-      @define-color base00 #${config.lib.stylix.colors.base00};
-      @define-color base01 #${config.lib.stylix.colors.base01};
-      @define-color base02 #${config.lib.stylix.colors.base02};
-      @define-color base03 #${config.lib.stylix.colors.base03};
-      @define-color base04 #${config.lib.stylix.colors.base04};
-      @define-color base05 #${config.lib.stylix.colors.base05};
-      @define-color base06 #${config.lib.stylix.colors.base06};
-      @define-color base07 #${config.lib.stylix.colors.base07};
-      @define-color base08 #${config.lib.stylix.colors.base08};
-      @define-color base09 #${config.lib.stylix.colors.base09};
-      @define-color base0A #${config.lib.stylix.colors.base0A};
-      @define-color base0B #${config.lib.stylix.colors.base0B};
-      @define-color base0C #${config.lib.stylix.colors.base0C};
-      @define-color base0D #${config.lib.stylix.colors.base0D};
-      @define-color base0E #${config.lib.stylix.colors.base0E};
-      @define-color base0F #${config.lib.stylix.colors.base0F};
+    style = with config.lib.stylix.colors; lib.mkForce /* css */ ''
+      @define-color base00 #${base00};
+      @define-color base01 #${base01};
+      @define-color base02 #${base02};
+      @define-color base03 #${base03};
+      @define-color base04 #${base04};
+      @define-color base05 #${base05};
+      @define-color base06 #${base06};
+      @define-color base07 #${base07};
+      @define-color base08 #${base08};
+      @define-color base09 #${base09};
+      @define-color base0A #${base0A};
+      @define-color base0B #${base0B};
+      @define-color base0C #${base0C};
+      @define-color base0D #${base0D};
+      @define-color base0E #${base0E};
+      @define-color base0F #${base0F};
 
       * {
         color: @base05;
