143443490366229082baca0d287e724a8a0bf7ae
Donovan Glover <donovan@dglover.co>
2023-06-25 21:40:49 -0400

    fish: Set bat theme to base16

--- modules/fish.nix
+++ modules/fish.nix
@@ -8,6 +8,7 @@
     GIT_DISCOVERY_ACROSS_FILESYSTEM = "1";
     FZF_DEFAULT_OPTS = "--height 40% --reverse --border --color=16";
     NODE_OPTIONS = "--max_old_space_size=16384";
+    BAT_THEME = "base16";
   };
 
   environment.systemPackages = with pkgs; [
