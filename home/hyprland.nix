68b80d2a187fb96f301ea12904b3c8986ecde70c
Donovan Glover <donovan@dglover.co>
2023-06-30 08:06:02 -0400

    hyprland: Add borders by default
    This makes it easier to differentiate which window is focused and makes
    floating windows slightly prettier.

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -35,9 +35,9 @@ in
     }
 
     general {
-      gaps_in = 0
+      gaps_in = -1
       gaps_out = -1
-      border_size = 0
+      border_size = 2
       col.active_border = rgba(${base0A}ee) rgba(${base09}ee) 45deg
       col.inactive_border = rgba(${base02}aa)
       layout = master
