1c151062310fe421498703d0b1284631e8da9f5a
Donovan Glover <donovan@dglover.co>
2023-07-04 18:59:18 -0400

    tests: Remove joshuto check
    No longer needed since v0.9.5 was released.

--- tests/main.cr
+++ tests/main.cr
@@ -8,12 +8,6 @@ check_top_level_imports("modules")
 check_top_level_imports("overlays")
 check_top_level_imports("specializations")
 
-describe "./overlays/joshuto/default.nix", tags: "online" do
-  it "uses the latest joshuto commit" do
-    check_latest_commit("kamiyaa/joshuto", branch: "main")
-  end
-end
-
 describe "./overlays/rofi/default.nix", tags: "online" do
   it "uses the latest rofi-wayland commit" do
     check_latest_commit("lbonn/rofi", branch: "wayland")
