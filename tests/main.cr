44b6e345d174e25e1be4522a840c8645d1726754
Donovan Glover <donovan@dglover.co>
2023-07-06 21:43:02 -0400

    tests: Remove rofi check

--- tests/main.cr
+++ tests/main.cr
@@ -7,9 +7,3 @@ check_top_level_imports("home")
 check_top_level_imports("modules")
 check_top_level_imports("overlays")
 check_top_level_imports("specializations")
-
-describe "./overlays/rofi/default.nix", tags: "online" do
-  it "uses the latest rofi-wayland commit" do
-    check_latest_commit("lbonn/rofi", branch: "wayland")
-  end
-end
