a3ca695d4e7a7874b5f1f37110ec0916935751bf
Donovan Glover <donovan@dglover.co>
2023-07-09 22:25:34 -0400

    tests: Remove hint
    At some point this broke, although imports should be changed
    infrequently enough that any failures should be trivial to resolve.

--- tests/check_top_level_imports.cr
+++ tests/check_top_level_imports.cr
@@ -1,8 +1,6 @@
 require "spec"
 require "colorize"
 
-hint = ""
-
 def check_top_level_imports(directory)
   describe "./#{directory}/default.nix" do
     it "imports all modules in ./#{directory}/", tags: "local" do
@@ -11,11 +9,8 @@ def check_top_level_imports(directory)
       modules = File.read("./#{directory}/default.nix")
 
       all_modules.each do |current_module|
-        hint = "Missing ./#{current_module} import in ./#{directory}/default.nix."
         modules.includes?("./#{current_module}").should be_true
       end
-
-      hint = ""
     end
 
     it "only imports modules that exist in ./#{directory}/", tags: "local" do
@@ -25,16 +20,9 @@ def check_top_level_imports(directory)
       File.each_line("./#{directory}/default.nix") do |line|
         if line.includes? "./"
           imported_file = line.lstrip(' ').lstrip("./")
-          hint = "./#{imported_file} was imported but doesn't exist in directory ./#{directory}/."
           all_modules.includes?(imported_file).should be_true
         end
       end
-
-      hint = ""
     end
   end
-end
-
-Spec.after_suite do
-  hint.empty? || puts "✗ #{hint}".colorize(:yellow)
 end
