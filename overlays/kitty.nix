19ffaad1cfd0c4f6a2b9130689c46b8fe217cffc
Donovan Glover <donovan@dglover.co>
2023-07-11 14:24:13 -0400

    meta: Remove kitty overlay
    Not needed anymore since kitty 0.29.0 was released.

--- overlays/kitty.nix
+++
@@ -1,9 +0,0 @@
-{
-  nixpkgs.overlays = [
-    (final: prev: {
-      kitty = prev.kitty.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [ ../patches/kitty-fix-duplicate-lines.patch ];
-      });
-    })
-  ];
-}
