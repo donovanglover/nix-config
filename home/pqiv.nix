35c502e22ff6f3368222eea2f0f3817f84b3ff6c
Donovan Glover <donovan@dglover.co>
2023-07-15 12:03:51 -0400

    pqiv: Use native module

--- home/pqiv.nix
+++ home/pqiv.nix
@@ -1,11 +1,16 @@
 {
-  xdg.configFile."pqivrc".text = /* ini */ ''
-    [options]
-    lazy-load=1
-    hide-info-box=1
-    background-pattern=black
-    disable-backends=archive,archive_cbx,libav,poppler,spectre,wand,webp
-    thumbnail-size=256x256
-    command-1=thunar
-  '';
+  programs.pqiv = {
+    enable = true;
+
+    settings = {
+      options = {
+        lazy-load = 1;
+        hide-info-box = 1;
+        background-pattern = "black";
+        disable-backends = "archive,archive_cbx,libav,poppler,spectre,wand,webp";
+        thumbnail-size = "256x256";
+        command-1 = "thunar";
+      };
+    };
+  };
 }
