38a817b9ff7f7805657ab9a0c823e6dfa311830c
Donovan Glover <donovan@dglover.co>
2023-07-26 09:57:52 -0400

    pqiv: Enable support for svg and other images
    This change enables support for all formats imagemagick supports.

--- home/pqiv.nix
+++ home/pqiv.nix
@@ -7,7 +7,7 @@
         lazy-load = 1;
         hide-info-box = 1;
         background-pattern = "black";
-        disable-backends = "archive,archive_cbx,libav,poppler,spectre,wand,webp";
+        disable-backends = "archive,archive_cbx,libav,poppler,spectre,webp";
         thumbnail-size = "256x256";
         command-1 = "thunar";
       };
