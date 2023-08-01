4e4ceeb6fb83ffa08c38748d7780bb0bd14d5dca
Donovan Glover <donovan@dglover.co>
2023-08-01 12:15:58 -0400

    feat(neovim): Highlight markdown code blocks
    This adds syntax highlighting to code blocks with vim-markdown while
    keeping pretty much everything else the same.

--- home/neovim.nix
+++ home/neovim.nix
@@ -387,6 +387,16 @@ in
         '';
       }
       {
+        plugin = vim-markdown;
+        config = /* vim */ ''
+          let g:vim_markdown_folding_disabled = 1
+          let g:vim_markdown_conceal = 0
+          let g:vim_markdown_frontmatter = 1
+          let g:vim_markdown_toml_frontmatter = 1
+          let g:vim_markdown_json_frontmatter = 1
+        '';
+      }
+      {
         plugin = alpha-nvim;
         type = "lua";
         config = /* lua */ ''
