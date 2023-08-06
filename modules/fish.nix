ec07140d4a396f1237e3e561e4e1e40b424f0073
Donovan Glover <donovan@dglover.co>
2023-08-06 16:50:05 -0400

    fish: Add cargo add/remove --dev abbreviations

--- modules/fish.nix
+++ modules/fish.nix
@@ -98,12 +98,14 @@
       cu = "cargo update";
       ca = "cargo add";
       cab = "cargo add --build";
+      cad = "cargo add --dev";
       cb = "cargo build";
       cr = "cargo run";
       cs = "cargo search";
       cn = "cargo new";
       crm = "cargo remove";
       crmb = "cargo remove --build";
+      crmd = "cargo remove --dev";
       cc = "cargo clippy";
       cf = "cargo fmt";
 
