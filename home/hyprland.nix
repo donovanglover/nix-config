cfd638b04e27f6dab8275ae6e0f2d4ea8ff5ac2b
Donovan Glover <donovan@dglover.co>
2023-08-07 22:18:13 -0400

    hyprland: Show desktop icons on startup

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -74,6 +74,7 @@ in
     exec-once = hyprctl dispatch workspace 5000000
     exec-once = ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1
     exec-once = hyprdim --no-dim-when-only --persist --ignore-leaving-special
+    exec-once = sleep 1 && eww open desktop-icons
     exec-once = ~/.config/hypr/random-bg.fish
 
     input {
