ade6fa196ba437a2837bf57f6aff32b93b0a4745
Donovan Glover <donovan@dglover.co>
2023-08-10 20:37:21 -0400

    fish(exa): Show git status

--- modules/fish.nix
+++ modules/fish.nix
@@ -24,8 +24,8 @@
     enable = true;
 
     shellAliases = {
-      ls = "${pkgs.exa}/bin/exa --icons --group-directories-first -I 'lost+found'";
-      tree = "${pkgs.exa}/bin/exa --icons --group-directories-first --all --long --tree -I 'node_modules|.git|public|lost+found|target|.next'";
+      ls = "${pkgs.exa}/bin/exa --icons --group-directories-first --git -I 'lost+found'";
+      tree = "${pkgs.exa}/bin/exa --icons --group-directories-first --all --long --tree --git -I 'node_modules|.git|public|lost+found|target|.next'";
       mv = "mv -i";
       cp = "cp -ia";
       rg = "${pkgs.ripgrep}/bin/rg --max-columns=2000 --smart-case";
