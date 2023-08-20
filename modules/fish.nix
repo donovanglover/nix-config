917b7d626a4b49f1c5c9f2f06f8359a41f994756
Donovan Glover <donovan@dglover.co>
2023-08-19 22:15:53 -0400

    fish(eza): Ignore .nuxt directory

--- modules/fish.nix
+++ modules/fish.nix
@@ -26,7 +26,7 @@
 
     shellAliases = {
       ls = "${pkgs.eza}/bin/eza --icons --group-directories-first -I 'lost+found'";
-      tree = "${pkgs.eza}/bin/eza --icons --group-directories-first --all --long --tree -I 'node_modules|.git|public|lost+found|target|.next|.cache'";
+      tree = "${pkgs.eza}/bin/eza --icons --group-directories-first --all --long --tree -I 'node_modules|.git|public|lost+found|target|.next|.cache|.nuxt'";
       mv = "mv -i";
       cp = "cp -ia";
       rg = "${pkgs.ripgrep}/bin/rg --max-columns=2000 --smart-case";
