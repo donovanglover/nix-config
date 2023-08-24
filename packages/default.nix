f2aa0efe21f86e927b27ae53397e485fd239a8a4
Donovan Glover <donovan@dglover.co>
2023-08-24 17:25:24 -0400

    feat: Add thumbnail support for webp files

--- packages/default.nix
+++ packages/default.nix
@@ -4,10 +4,12 @@
   environment.pathsToLink = [
     "/share/backgrounds"
     "/share/eww"
+    "/share/thumbnailers"
   ];
 
   environment.systemPackages = [
     (pkgs.callPackage ./osu-backgrounds.nix { })
     (pkgs.callPackage ./fluent-icons.nix { })
+    (pkgs.callPackage ./webp-thumbnailer.nix { })
   ];
 }
---
+++ packages/webp-thumbnailer.nix
@@ -0,0 +1,23 @@
+{ stdenvNoCC
+, libwebp
+}:
+
+stdenvNoCC.mkDerivation {
+  pname = "webp-thumbnailer";
+  version = "1.0.0";
+
+  src = ../assets;
+
+  postInstall = /* bash */ ''
+    mkdir -p $out/share/thumbnailers
+
+    echo "[Thumbnailer Entry]"                            >> $out/share/thumbnailers/webp.thumbnailer
+    echo "Exec=${libwebp}/bin/dwebp %i -scale %s 0 -o %o" >> $out/share/thumbnailers/webp.thumbnailer
+    echo "MimeType=image/x-webp;image/webp;"              >> $out/share/thumbnailers/webp.thumbnailer
+  '';
+
+  meta = {
+    homepage = "https://github.com/liquuid/nautilus-webp-thumbnailer";
+    description = "Create thumbnails from webp files";
+  };
+}
