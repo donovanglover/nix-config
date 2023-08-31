f315ce42f6411392995633f942de812cccd8ea30
Donovan Glover <donovan@dglover.co>
2023-08-31 11:51:44 -0400

    chore: Run deno fmt

--- tests/lib.ts
+++ tests/lib.ts
@@ -6,33 +6,36 @@ async function getFilesInDirectory(directory: string): Promise<string[]> {
 
   for await (const walkEntry of walk(directory)) {
     if (walkEntry.isFile) {
-      if (walkEntry.path.includes("default.nix")) continue
-      files.push(walkEntry.path)
+      if (walkEntry.path.includes("default.nix")) continue;
+      files.push(walkEntry.path);
     }
   }
 
-  return files
+  return files;
 }
 
 async function getImportsInFile(file: string): Promise<string[]> {
   const text = await Deno.readTextFile(file);
-  const lines = text.split("\n")
+  const lines = text.split("\n");
   const imports = [];
 
   for (let i = 0; i < lines.length; i++) {
     if (lines[i].includes("./")) {
-      imports.push(file.split("./")[1].split("/")[0] + "/" + lines[i].split("./")[1].split(" ")[0])
+      imports.push(
+        file.split("./")[1].split("/")[0] + "/" +
+          lines[i].split("./")[1].split(" ")[0],
+      );
     }
   }
 
-  return imports
+  return imports;
 }
 
 export async function assertAllModulesInDirectory(directory: string) {
-  const files = await getFilesInDirectory(`./${directory}`)
-  const imports = await getImportsInFile(`./${directory}/default.nix`)
+  const files = await getFilesInDirectory(`./${directory}`);
+  const imports = await getImportsInFile(`./${directory}/default.nix`);
 
   for (const file of files) {
-    assert(imports.includes(file))
+    assert(imports.includes(file));
   }
 }
--- tests/main.ts
+++ tests/main.ts
@@ -1,9 +1,16 @@
 import { assertAllModulesInDirectory } from "./lib.ts";
 
-const dirs = ["containers", "home", "modules", "overlays", "packages", "specializations"]
+const dirs = [
+  "containers",
+  "home",
+  "modules",
+  "overlays",
+  "packages",
+  "specializations",
+];
 
 for (const dir of dirs) {
   Deno.test(`imports all modules in ./${dir}`, async () => {
-    await assertAllModulesInDirectory(dir)
-  })
+    await assertAllModulesInDirectory(dir);
+  });
 }
