e977687af6ae47a2db317e65078afb82f672de52
Donovan Glover <donovan@dglover.co>
2023-10-25 13:22:10 -0400

    feat: Use upstream nixos-unstable branch
    Since my changes are now upstreamed, I no longer need to use a personal
    nixos-unstable branch. This makes it easier to use the source code of
    the repository and simplifies the updating process.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:donovanglover/nixpkgs/personal-unstable-new";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
