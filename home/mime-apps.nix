e397e85c306903a90ade947f64d8b7eb8b064a0d
Donovan Glover <donovan@dglover.co>
2023-10-30 19:34:04 -0400

    chore: Upstream sakaya mime type
    Makes it possible to open wine applications sandboxed by double clicking
    them like regular files in Thunar and other file browsers.

--- home/mime-apps.nix
+++ home/mime-apps.nix
@@ -7,7 +7,6 @@
       "image/jpeg" = "pqiv.desktop";
       "image/gif" = "org.qutebrowser.qutebrowser.desktop";
       "application/x-wine-extension-osz" = "osu-stable.desktop";
-      "application/x-ms-dos-executable" = "sakaya.desktop";
       "x-scheme-handler/http" = "librewolf.desktop";
       "x-scheme-handler/https" = "librewolf.desktop";
     };
