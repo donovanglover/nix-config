5c2625e4e22f84eb38f50434c2d052f91b39cf47
Donovan Glover <donovan@dglover.co>
2023-11-30 18:56:51 -0500

    fish: Add alias for npm-check-updates
    A good replacement for yarn upgrade-interactive.

--- modules/fish.nix
+++ modules/fish.nix
@@ -31,6 +31,7 @@
       cp = "cp -ia";
       rg = "${pkgs.ripgrep}/bin/rg --max-columns=2000 --smart-case";
       yarn = "${pkgs.yarn}/bin/yarn --emoji true";
+      ncu = "${pkgs.npm-check-updates}/bin/ncu --interactive --format group";
     };
 
     shellAbbrs = {
