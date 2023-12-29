ac3699fd6ed9a057455fddf761ced469c32ef108
Donovan Glover <donovan@dglover.co>
2023-12-29 18:10:24 -0500

    mpv: Drop gpu-hq profile for now
    This setting causes some videos to experience lagginess on my iGPU. In
    the future it may be useful to enable the new profile "high-quality" if
    I have a dedicated GPU.
    
    Note that in addition to setting the profile to high-quality, it's also
    possible to use "vo=gpu-next".
    
    See: https://github.com/mpv-player/mpv/commit/703f1588803eaa428e09c0e5547b26c0fff476a7

--- home/mpv.nix
+++ home/mpv.nix
@@ -5,9 +5,6 @@
     enable = true;
 
     config = {
-      profile = "gpu-hq";
-      scale = "ewa_lanczossharp";
-      cscale = "ewa_lanczossharp";
       video-sync = "display-resample";
       interpolation = true;
       tscale = "oversample";
