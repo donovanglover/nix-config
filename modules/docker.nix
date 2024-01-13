4a7d310d9fcc94fbf4d060f66d0c36bf3259446c
Donovan Glover <donovan@dglover.co>
2024-01-13 00:31:28 -0500

    docker: Drop podman
    This fixes an issue where podman was causing NixOS virtual machines
    (which depend on QEMU) to not start with the following error:
    
    qemu-kvm: could not stat pidfile /run/user/1000/pulse/pid: No such file or directory

--- modules/docker.nix
+++ modules/docker.nix
@@ -3,7 +3,6 @@
 {
   virtualisation.docker.enable = true;
   virtualisation.docker.enableOnBoot = false;
-  virtualisation.podman.enable = true;
 
   environment.systemPackages = with pkgs; [
     docker-compose
