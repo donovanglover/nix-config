704f9d6be965c085be73f5c6e64f15b6715de185
Donovan Glover <donovan@dglover.co>
2024-01-29 18:10:53 -0500

    pqiv: Always scale 100% outside fullscreen

--- home/pqiv.nix
+++ home/pqiv.nix
@@ -6,6 +6,7 @@
       options = {
         lazy-load = 1;
         hide-info-box = 1;
+        scale-mode-screen-fraction = 1;
         background-pattern = "black";
         disable-backends = "archive,archive_cbx,libav,poppler,spectre";
         thumbnail-size = "256x256";
