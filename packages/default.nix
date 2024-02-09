00a246c9c6b3e019af6b22bf88d872f08befaeec
Donovan Glover <donovan@dglover.co>
2024-02-08 22:33:47 -0500

    packages: Add aleo-fonts
    I've been using this for a few days now and Aleo is my new favorite
    serif font.

--- packages/default.nix
+++ packages/default.nix
@@ -5,9 +5,11 @@
     "/share/backgrounds"
     "/share/eww"
     "/share/thumbnailers"
+    "/share/fonts"
   ];
 
   environment.systemPackages = with pkgs; [
+    (callPackage ./aleo-fonts.nix { })
     (callPackage ./fluent-icons.nix { })
     (callPackage ./osu-backgrounds.nix { })
     (callPackage ./pnpm-shell-completion.nix { })
