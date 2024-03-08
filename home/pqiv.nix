3a46bb767fe04c0e544567e7f88c9073f489209b
Donovan Glover <donovan@dglover.co>
2024-03-08 00:00:55 -0500

    pqiv: Set max-depth to 0
    Fixes an issue where the latest version of pqiv would become
    unresponsive when browsing images in directories with large
    subdirectories. Also makes selecting multiple images work again.
    
    See: https://github.com/phillipberndt/pqiv/pull/204

--- home/pqiv.nix
+++ home/pqiv.nix
@@ -7,6 +7,7 @@
         lazy-load = 1;
         hide-info-box = 1;
         scale-mode-screen-fraction = 1;
+        max-depth = 0;
         background-pattern = "black";
         disable-backends = "archive,archive_cbx,libav,poppler,spectre";
         thumbnail-size = "256x256";
