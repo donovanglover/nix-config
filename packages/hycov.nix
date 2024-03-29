647746b8bc20670cbfb54667e5779337aeaa9ccf
Donovan Glover <donovan@dglover.co>
2024-03-29 15:20:05 -0400

    chore: Cleanup hycov package
    Note that the new hyprlandPlugins.mkHyprlandPlugin does not appear to
    work here, which is why it wasn't used.

--- packages/hycov.nix
+++ packages/hycov.nix
@@ -3,18 +3,27 @@
   stdenv,
   hyprland,
   fetchFromGitHub,
+  fetchpatch,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "hycov";
-  version = "0.1";
+  version = "0.36.0.1";
 
   src = fetchFromGitHub {
-    owner = "Ayuei";
+    owner = "DreamMaoMao";
     repo = "hycov";
-    rev = "db6ea8a24f0a58fa69f86db49d7c853754b1b8e1";
-    hash = "sha256-fSujEAViKqQusHyYe7Qg8QnLlyUNOf0/Jk1P70BmiyE=";
+    rev = finalAttrs.version;
+    hash = "sha256-GHrgunfo+UARnf3OgNhdXoNFALRAmQEGhlZ8x5TBvkQ=";
   };
+
+  patches = [
+    # Update method signatures for Hyprland 0.37.0+
+    (fetchpatch {
+      url = "https://github.com/Ayuei/hycov/commit/db6ea8a24f0a58fa69f86db49d7c853754b1b8e1.patch";
+      hash = "sha256-2FMHLmxuuBrui57d6kw6sfJJmtAV0vNF8sjFdaeIgf0=";
+    })
+  ];
 
   inherit (hyprland) nativeBuildInputs;
 
@@ -26,4 +35,4 @@ stdenv.mkDerivation {
     license = licenses.mit;
     platforms = platforms.linux;
   };
-}
+})
