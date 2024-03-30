f1a5aa85206c7ec5b9e2446d9f0b78af3d90de4e
Donovan Glover <donovan@dglover.co>
2024-03-30 08:12:22 -0400

    neovim: Add border to nvim-lspconfig popups
    This makes it easier to differentiate code from lsp popups.

--- home/neovim.nix
+++ home/neovim.nix
@@ -294,6 +294,18 @@ in
               vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
             end,
           })
+
+          vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+            vim.lsp.handlers.hover, { border = "single" }
+          )
+
+          vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+            vim.lsp.handlers.signature_help, { border = "single" }
+          )
+
+          vim.diagnostic.config {
+            float = { border = "single" }
+          }
         '';
       }
       {
