12464e8b65664795d2d8e8de66f15f4278a8e4c1
Donovan Glover <donovan@dglover.co>
2024-04-01 05:50:42 -0400

    chore: Improve formatting
    No need to write stylix so many times.

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -6,47 +6,49 @@ let
   font-size = 11;
 in
 {
-  stylix.image = ../assets/wallpaper.png;
-  stylix.polarity = "dark";
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
+  stylix = {
+    image = ../assets/wallpaper.png;
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
 
-  stylix.opacity = {
-    terminal = opacity;
-    popups = opacity;
-  };
-
-  stylix.cursor = with pkgs; {
-    package = phinger-cursors;
-    name = "phinger-cursors";
-    size = 24;
-  };
-
-  stylix.fonts = with pkgs; {
-    serif = {
-      package = (callPackage ../packages/aleo-fonts.nix { });
-      name = "Aleo";
+    opacity = {
+      terminal = opacity;
+      popups = opacity;
     };
 
-    sansSerif = {
-      package = noto-fonts-cjk-sans;
-      name = "Noto Sans CJK JP";
+    cursor = with pkgs; {
+      package = phinger-cursors;
+      name = "phinger-cursors";
+      size = 24;
     };
 
-    monospace = {
-      package = maple-mono;
-      name = "Maple Mono";
-    };
+    fonts = with pkgs; {
+      serif = {
+        package = (callPackage ../packages/aleo-fonts.nix { });
+        name = "Aleo";
+      };
 
-    emoji = {
-      package = noto-fonts-emoji;
-      name = "Noto Color Emoji";
-    };
+      sansSerif = {
+        package = noto-fonts-cjk-sans;
+        name = "Noto Sans CJK JP";
+      };
 
-    sizes = {
-      applications = font-size;
-      desktop = font-size;
-      popups = font-size;
-      terminal = font-size;
+      monospace = {
+        package = maple-mono;
+        name = "Maple Mono";
+      };
+
+      emoji = {
+        package = noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+
+      sizes = {
+        applications = font-size;
+        desktop = font-size;
+        popups = font-size;
+        terminal = font-size;
+      };
     };
   };
 }
