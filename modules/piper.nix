039cf9736830d537b87aae958b11b6de59829aa7
Donovan Glover <donovan@dglover.co>
2024-04-03 06:29:04 -0400

    meta: Merge udev with piper
    This is part of merging all hardware-specific config into one module.

--- modules/piper.nix
+++ modules/piper.nix
@@ -4,4 +4,8 @@
   services.ratbagd.enable = true;
 
   environment.systemPackages = [ pkgs.piper ];
+
+  services.udev.extraRules = ''
+    KERNEL=="event*", ATTRS{name}=="AT Translated Set 2 keyboard", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+  '';
 }
--- modules/udev.nix
+++
@@ -1,5 +0,0 @@
-{
-  services.udev.extraRules = ''
-    KERNEL=="event*", ATTRS{name}=="AT Translated Set 2 keyboard", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-  '';
-}
