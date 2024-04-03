9ccdacaadf8e88ba1494fe397c9cb9d97de03004
Donovan Glover <donovan@dglover.co>
2024-04-03 10:12:54 -0400

    chore: Simplify htop module

--- home/htop.nix
+++ home/htop.nix
@@ -1,18 +1,22 @@
 { pkgs, ... }:
 
+let
+  inherit (pkgs) htop-vim;
+in
 {
   programs.htop = {
     enable = true;
-    package = pkgs.htop-vim;
+    package = htop-vim;
 
     settings = {
-      tree_view = 1;
-      hide_userland_threads = 1;
-      highlight_changes = 1;
-      show_cpu_frequency = 1;
-      show_cpu_temperature = 1;
-      show_program_path = 0;
-      highlight_base_name = 1;
+      tree_view = true;
+      hide_userland_threads = true;
+      highlight_changes = true;
+      show_cpu_frequency = true;
+      show_cpu_temperature = true;
+      highlight_base_name = true;
+
+      show_program_path = false;
     };
   };
 }
