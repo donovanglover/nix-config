85ead9e77994bed87d4aca0ef0716372448b0232
Donovan Glover <donovan@dglover.co>
2024-04-03 07:30:40 -0400

    meta: Merge boot module with system
    This seems like it could fit with the hardware module as well, however
    time will tell if we're able to keep this in system when importing it
    into containers and virtual machines.
    
    Note that boot.loader.efi.canTouchEfiVariables gets set to true during
    the nixos-install process, so it should be okay to keep here.

--- modules/system.nix
+++ modules/system.nix
@@ -1,4 +1,15 @@
 {
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      editor = false;
+      configurationLimit = 10;
+    };
+
+    timeout = 0;
+    efi.canTouchEfiVariables = true;
+  };
+
   time.timeZone = "America/New_York";
 
   i18n.defaultLocale = "ja_JP.UTF-8";
