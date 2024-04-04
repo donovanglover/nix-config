e64cc2697f0f4107bcee14e1a59fe817f088c10f
Donovan Glover <donovan@dglover.co>
2024-04-04 11:25:25 -0400

    meta: Migrate yazi from home-manager to shell module
    This is a part of making it easier to instantly have access to yazi
    without having to worry about using home-manager. Note that this works
    for my use case since I don't use Nix on non-NixOS devices and don't
    intend to do so anytime soon.

--- modules/shell.nix
+++ modules/shell.nix
@@ -274,5 +274,35 @@ in
         show_program_path = false;
       };
     };
+
+    yazi = {
+      enable = true;
+
+      settings = {
+        yazi = {
+          manager = {
+            ratio = [ 1 2 4 ];
+            sort_dir_first = true;
+          };
+
+          preview = {
+            tab_size = 4;
+            image_filter = "lanczos3";
+            max_width = 1920;
+            max_height = 1080;
+            image_quality = 90;
+          };
+        };
+
+        keymap = {
+          manager.prepend_keymap = [
+            {
+              run = "remove --force";
+              on = [ "d" ];
+            }
+          ];
+        };
+      };
+    };
   };
 }
