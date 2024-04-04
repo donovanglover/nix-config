38e82be3808ff50c2f713a840f28e9ce2e71f375
Donovan Glover <donovan@dglover.co>
2024-04-03 21:39:13 -0400

    meta: Merge pipewire module with hyprland
    Realistically if hyprland is being used then the user *probably* wants
    audio as well, so merging them makes sense here.
    
    This commit is a part of ultimately having a single module for "desktop"
    usage.

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -21,4 +21,19 @@
     enable = true;
     excludePackages = [ pkgs.xterm ];
   };
+
+  services.pipewire = {
+    enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+
+    pulse.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    pulseaudio
+  ];
 }
