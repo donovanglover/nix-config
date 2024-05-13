c193b2b2cf5f4e9f522eb94fd5985f26fa14ed9e
Donovan Glover <donovan@dglover.co>
2024-05-13 09:20:58 -0400

    overlays: Overlay neovim to make nvim.desktop work in thunar

---
+++ overlays/neovim-unwrapped.nix
@@ -0,0 +1,10 @@
+final: prev: {
+  neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (oldAttrs: {
+    postInstall = (oldAttrs.postInstall or "") + ''
+      substituteInPlace $out/share/applications/nvim.desktop \
+        --replace "TryExec=nvim" "" \
+        --replace "Terminal=true" "Terminal=false" \
+        --replace "Exec=nvim %F" "Exec=kitty nvim %F"
+    '';
+  });
+}
