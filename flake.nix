576b9bf2baea828534fac7c0199fe6bd169dd947
Donovan Glover <donovan@dglover.co>
2024-06-01 14:01:56 -0400

    home-manager: Lock version to 24.05
    We'll be staying on the 24.05 release for the time being since we
    haven't updated nixos-unstable to 24.11 yet.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
