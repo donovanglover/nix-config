4f19c5df14c6e8011e0a6b1ccacf2c6b467e0728
Donovan Glover <donovan@dglover.co>
2024-06-02 10:53:45 -0400

    zathura: Fix opaque highlight color
    Fixes an issue where newer versions of zathura changed how colors were
    specified. Should eventually be fixed upstream in stylix, but we're
    using the Kvantum fork for theming support in Qt applications.
    
    Based on the upstream pull request with a preference for 0.3 highlight
    transparency over 0.5.

--- home/zathura.nix
+++ home/zathura.nix
@@ -1,3 +1,12 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkForce;
+
+  highlightTransparency = "0.3";
+  getColorCh = colorName: channel: config.lib.stylix.colors."${colorName}-rgb-${channel}";
+  rgba = color: ''rgba(${getColorCh color "r"}, ${getColorCh color "g"}, ${getColorCh color "b"}, ${highlightTransparency})'';
+in
 {
   programs.zathura = {
     enable = true;
@@ -8,6 +17,9 @@
       statusbar-basename = true;
       render-loading = false;
       scroll-step = 120;
+
+      highlight-color = mkForce (rgba "base0A");
+      highlight-active-color = mkForce (rgba "base0D");
     };
   };
 }
