0e028019f6e993fe4e3ca650f532d1fbfe3c89b0
Donovan Glover <donovan@dglover.co>
2024-06-13 06:22:21 -0400

    phone: Simplify options

--- phone/configuration.nix
+++ phone/configuration.nix
@@ -1,41 +1,30 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  networking.hostName = "mobile-nixos";
+  networking = {
+    hostName = "mobile-nixos";
+    wireless.enable = false;
+    networkmanager.enable = true;
+  };
 
-  #
-  # Opinionated defaults
-  #
+  hardware = {
+    pulseaudio = {
+      enable = true;
+      package = pkgs.pulseaudioFull;
+    };
 
-  # Use Network Manager
-  networking.wireless.enable = false;
-  networking.networkmanager.enable = true;
+    bluetooth.enable = true;
+  };
 
-  # Use PulseAudio
-  hardware.pulseaudio.enable = true;
-
-  # Enable Bluetooth
-  hardware.bluetooth.enable = true;
-
-  # Bluetooth audio
-  hardware.pulseaudio.package = pkgs.pulseaudioFull;
-
-  # Enable power management options
   powerManagement.enable = true;
 
-  # It's recommended to keep enabled on these constrained devices
   zramSwap.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" "repl-flake" ];
 
-  # Auto-login for phosh
   services.xserver.desktopManager.phosh = {
     user = "user";
   };
-
-  #
-  # User configuration
-  #
 
   users.users."user" = {
     isNormalUser = true;
@@ -50,11 +39,5 @@
     ];
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11";
 }
