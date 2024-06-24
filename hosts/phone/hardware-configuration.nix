8af4d55361762fe7c112e959d5d0f5de11a0448f
Donovan Glover <donovan@dglover.co>
2024-06-24 12:59:45 -0400

    phone: Update device UUIDs
    Necessary since we had to reinstall Mobile NixOS due to being unable to
    recover from a chroot.

--- hosts/phone/hardware-configuration.nix
+++ hosts/phone/hardware-configuration.nix
@@ -3,14 +3,14 @@
 {
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/63546625-e99c-4aa4-ba11-ff193ad13047";
+      device = "/dev/disk/by-uuid/0a242de8-703e-46c1-a419-56109833aef5";
       fsType = "ext4";
     };
   };
 
   boot.initrd.luks.devices = {
-    "LUKS-MOBILE-NIXOS-ROOTFS" = {
-      device = "/dev/disk/by-uuid/28e0bd73-e4fb-4002-9b17-a494823e6999";
+    "LUKS-IPHONE-ROOTFS" = {
+      device = "/dev/disk/by-uuid/969741b3-6cc3-4667-92e8-5f0240253e6f";
     };
   };
 
