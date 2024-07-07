c9b79ccfb271256627410893977ded2cf7792aa0
Donovan Glover <donovan@dglover.co>
2024-07-07 18:51:55 -0400

    packages: Add nadesiko3
    Adds the Japanese programming language なでしこ. Note that an upstream
    lockfile would be ideal before merging the PR in nixpkgs.

---
+++ packages/nadesiko3.nix
@@ -0,0 +1,30 @@
+{ lib
+, buildNpmPackage
+, fetchurl
+}:
+
+buildNpmPackage rec {
+  pname = "nadesiko3";
+  version = "3.6.7";
+
+  src = fetchurl {
+    url = "https://registry.npmjs.org/nadesiko3/-/nadesiko3-${version}.tgz";
+    hash = "sha256-Y9kJErYwFgGCpL5uhXKOUmpheI2cwC4Rt5uHqoFIhTc=";
+  };
+
+  npmDepsHash = "sha256-9HGoX+0xzw6ukrR4umCU+Gk9InmKgJ2CztASgD2kXdo=";
+
+  postPatch = ''
+    ln -s ${../assets/nadesiko3-package-lock.json} package-lock.json
+  '';
+
+  dontNpmBuild = true;
+
+  meta = {
+    description = "Japanese Programming Language Nadesiko v3 (JavaScript/TypeScript)";
+    homepage = "https://nadesi.com";
+    license = lib.licenses.mit;
+    mainProgram = "cnako3";
+    maintainers = with lib.maintainers; [ donovanglover ];
+  };
+}
