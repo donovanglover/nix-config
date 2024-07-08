8488ec962fe416e7f9ef1cf198c609a1bbccac70
Donovan Glover <donovan@dglover.co>
2024-07-07 20:38:42 -0400

    packages: Add hyprfocus
    Builds but haven't tested yet.

---
+++ packages/hyprfocus.nix
@@ -0,0 +1,42 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, hyprland
+, pkg-config
+}:
+
+stdenv.mkDerivation {
+  pname = "hyprfocus";
+  version = "0-unstable-2024-05-30";
+
+  src = fetchFromGitHub {
+    owner = "pyt0xic";
+    repo = "hyprfocus";
+    rev = "aa7262d3a4564062f97b9cfdad47fd914cfb80f2";
+    hash = "sha256-R1ZgNhQkoS6ZHRRKB+j5vYgRANfYO//sHbrD7moUTx0=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    hyprland
+  ];
+
+  buildInputs = [ hyprland ] ++ hyprland.buildInputs;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib
+    mv hyprfocus.so $out/lib/libhyprfocus.so
+
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://github.com/pyt0xic/hyprfocus";
+    description = "Focus animation plugin for Hyprland inspired by Flashfocus";
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ donovanglover ];
+    platforms = lib.platforms.linux;
+  };
+}
