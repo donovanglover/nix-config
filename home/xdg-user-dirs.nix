9a3c1976ab4b8bce0ac9634f412049e3712c394f
Donovan Glover <donovan@dglover.co>
2024-07-14 09:22:18 -0400

    Revert "xdg: Hide desktop entries with home-manager"
    As alluded to previously I'd rather whitelist applications that show in
    a launcher than exclude them.

--- home/xdg-user-dirs.nix
+++ home/xdg-user-dirs.nix
@@ -4,49 +4,20 @@ let
   inherit (config.home) homeDirectory;
 in
 {
-  xdg = {
-    desktopEntries = {
-      htop = {
-        name = "htop";
-        noDisplay = true;
-      };
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
 
-      fish = {
-        name = "fish";
-        noDisplay = true;
-      };
+    desktop = null;
+    templates = null;
+    publicShare = null;
 
-      nvim = {
-        name = "nvim";
-        noDisplay = true;
-      };
-
-      yazi = {
-        name = "yazi";
-        noDisplay = true;
-      };
-
-      tectonic = {
-        name = "tectonic";
-        noDisplay = true;
-      };
-    };
-
-    userDirs = {
-      enable = true;
-      createDirectories = true;
-
-      desktop = null;
-      templates = null;
-      publicShare = null;
-
-      download = "${homeDirectory}/ダウンロード";
-      documents = "${homeDirectory}/ドキュメント";
-      music = "${homeDirectory}/音楽";
-      pictures = "${homeDirectory}/画像";
-      videos = "${homeDirectory}/ビデオ";
-    };
-
-    configFile."user-dirs.locale".text = "ja_JP";
+    download = "${homeDirectory}/ダウンロード";
+    documents = "${homeDirectory}/ドキュメント";
+    music = "${homeDirectory}/音楽";
+    pictures = "${homeDirectory}/画像";
+    videos = "${homeDirectory}/ビデオ";
   };
+
+  xdg.configFile."user-dirs.locale".text = "ja_JP";
 }
