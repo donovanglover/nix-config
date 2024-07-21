d2fe48e8d2123a43a671976ac620565b7aec7141
Donovan Glover <donovan@dglover.co>
2024-07-21 13:18:36 -0400

    chromium: Set webrtc-ip-handling-policy to public interface
    Seems to fix an issue where WebRTC was previously broken in
    ungoogled-chromium. Note that `default_public_interface_only`
    is used instead of `default` to prevent local IP address leaks.

--- home/chromium.nix
+++ home/chromium.nix
@@ -12,6 +12,7 @@ in
       "--ozone-platform=wayland"
       "--enable-wayland-ime"
       "--extension-mime-request-handling=always-prompt-for-install"
+      "--webrtc-ip-handling-policy=default_public_interface_only"
     ];
   };
 }
