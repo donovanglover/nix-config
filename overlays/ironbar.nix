accf30ec0b5af7ee3142dd8b7b0dd6f0cd3e4d3c
Donovan Glover <donovan@dglover.co>
2024-08-04 11:34:38 -0400

    ironbar: Update to latest commit
    Also patches an issue where Mullvad VPN would crash ironbar if
    the tray module was enabled.

--- overlays/ironbar.nix
+++ overlays/ironbar.nix
@@ -1,19 +1,21 @@
 final: prev: {
   ironbar = prev.ironbar.overrideAttrs (oldAttrs: rec {
-    version = "0.15.1-unstable-2024-07-13";
+    version = "0.15.1-unstable-2024-08-04";
 
     src = prev.fetchFromGitHub {
       owner = "JakeStanger";
       repo = "ironbar";
-      rev = "58190ab079d00dd53babb72346f1da6e1cc9ac72";
-      hash = "sha256-Se+Pg81W8R+SFGFlhF1dU+NnMWSdLo3nC9TdPHa2IL4=";
+      rev = "92c690dcd14c21272f89bfde292546a2ee828e23";
+      hash = "sha256-YFarQSZEIFpA1/9eRK4tm88mZYvWGIaAgCEAjazBO38=";
     };
+
+    patches = (oldAttrs.patches or [ ]) ++ [ ../assets/ironbar-fix-tray-crash.patch ];
 
     cargoDeps = oldAttrs.cargoDeps.overrideAttrs (
       prev.lib.const {
         name = "${oldAttrs.pname}-${version}-vendor.tar.gz";
         inherit src;
-        outputHash = "sha256-VkSznG2REXNhUKEVWwqlfA7BF9zXC+fxTgNeRfYaHi4=";
+        outputHash = "sha256-wvjxKn2xS3qDJb7xVkLObr+Pz1djx+KHNSI66aonzpI=";
       }
     );
   });
