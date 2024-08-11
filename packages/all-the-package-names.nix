2e74b21642ac5027060fa2045bbe6554399deaf2
Donovan Glover <donovan@dglover.co>
2024-08-11 16:56:52 -0400

    packages(all-the-package-names): Run nix-update
    Surprisingly easy to update packages in flakes with nix-update.

--- packages/all-the-package-names.nix
+++ packages/all-the-package-names.nix
@@ -6,16 +6,16 @@
 
 buildNpmPackage rec {
   pname = "all-the-package-names";
-  version = "2.0.1490";
+  version = "2.0.1669";
 
   src = fetchFromGitHub {
     owner = "nice-registry";
     repo = "all-the-package-names";
     rev = "v${version}";
-    hash = "sha256-sUJtg/sxNWdkN90AmqVRo5g0BaD9ej/Ha6kJamwO5iA=";
+    hash = "sha256-p0pt/mZoudMIMkLTksKCkSEO7yrixHNqGGnonpEGrO4=";
   };
 
-  npmDepsHash = "sha256-xXC6HA7rILtOWcqWlGye883HZGwiaQws16mlJaDqDQE=";
+  npmDepsHash = "sha256-F5kJJ5oA69xmv9VXVpegjcLHEnRtzGL3DV2kAghfDqY=";
 
   meta = {
     description = "A list of all the public package names on npm";
