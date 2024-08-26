9aaf6ed972bbfbfe85e0fd0cb533cd8c4eb20120
Donovan Glover <donovan@dglover.co>
2024-08-25 22:33:15 -0400

    meta: Move editorconfig to separate module

---
+++ home/editorconfig.nix
@@ -0,0 +1,16 @@
+{
+  editorconfig = {
+    enable = true;
+
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        insert_final_newline = true;
+        indent_size = 2;
+        indent_style = "space";
+        trim_trailing_whitespace = false;
+      };
+    };
+  };
+}
--- home/neovim.nix
+++ home/neovim.nix
@@ -1,21 +1,6 @@
 { pkgs, ... }:
 
 {
-  editorconfig = {
-    enable = true;
-
-    settings = {
-      "*" = {
-        charset = "utf-8";
-        end_of_line = "lf";
-        insert_final_newline = true;
-        indent_size = 2;
-        indent_style = "space";
-        trim_trailing_whitespace = false;
-      };
-    };
-  };
-
   programs.neovim = {
     enable = true;
     defaultEditor = true;
