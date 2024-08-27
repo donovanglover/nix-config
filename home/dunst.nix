99fd8bf3f30861004134a8ed99b6f72f82660dff
Donovan Glover <donovan@dglover.co>
2024-08-27 15:19:15 -0400

    meta: Move dunst config out of dwm
    Long-term I'll have to decide if it's worth it to use two separate
    notification daemons for X and Wayland.

---
+++ home/dunst.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  services.dunst = {
+    enable = true;
+
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus";
+    };
+
+    settings = {
+      global = {
+        geometry = "1870x5-25+45";
+        width = 350;
+        separator_height = 5;
+        padding = 24;
+        horizontal_padding = 24;
+        frame_width = 3;
+        idle_threshold = 120;
+        alignment = "center";
+        word_wrap = "yes";
+        transparency = 5;
+        format = "<b>%s</b>: %b";
+        markup = "full";
+        min_icon_size = 32;
+        max_icon_size = 128;
+      };
+    };
+  };
+}
