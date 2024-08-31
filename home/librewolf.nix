92d9ed77b08ea015c4f7ca6f94a160b8b2696891
Donovan Glover <donovan@dglover.co>
2024-08-31 10:19:31 -0400

    librewolf: Configure settings at the profile level
    Fixes an issue where New Tab Identity wasn't being loaded on first
    launch of the profile due to settings not being profile-specific.
    
    Long-term this also makes it possible to take advantage of the "Show
    only modified preferences" feature, which showed our changes as the
    defaults before.

--- home/librewolf.nix
+++ home/librewolf.nix
@@ -49,34 +49,32 @@ in
 
       settings = {
         "extensions.autoDisableScopes" = 0;
+
+        "middlemouse.paste" = false;
+
+        "browser.download.useDownloadDir" = true;
+        "browser.tabs.insertAfterCurrent" = true;
+        "browser.tabs.warnOnClose" = true;
+        "browser.toolbars.bookmarks.visibility" = "never";
+        "browser.quitShortcut.disabled" = true;
+        "browser.sessionstore.restore_pinned_tabs_on_demand" = true;
+
+        "browser.urlbar.suggest.bookmark" = false;
+        "browser.urlbar.suggest.engines" = false;
+        "browser.urlbar.suggest.history" = false;
+        "browser.urlbar.suggest.openpage" = false;
+        "browser.urlbar.suggest.topsites" = false;
+        "browser.urlbar.trimHttps" = true;
+
+        "sidebar.position_start" = false;
+        "findbar.highlightAll" = true;
+
+        "xpinstall.signatures.required" = false;
+
+        "network.dns.native_https_query" = false;
+
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
-    };
-
-    settings = {
-      "middlemouse.paste" = false;
-
-      "browser.download.useDownloadDir" = true;
-      "browser.tabs.insertAfterCurrent" = true;
-      "browser.tabs.warnOnClose" = true;
-      "browser.toolbars.bookmarks.visibility" = "never";
-      "browser.quitShortcut.disabled" = true;
-      "browser.sessionstore.restore_pinned_tabs_on_demand" = true;
-
-      "browser.urlbar.suggest.bookmark" = false;
-      "browser.urlbar.suggest.engines" = false;
-      "browser.urlbar.suggest.history" = false;
-      "browser.urlbar.suggest.openpage" = false;
-      "browser.urlbar.suggest.topsites" = false;
-      "browser.urlbar.trimHttps" = true;
-
-      "sidebar.position_start" = false;
-      "findbar.highlightAll" = true;
-
-      "xpinstall.signatures.required" = false;
-
-      "network.dns.native_https_query" = false;
-
-      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
     };
   };
 
