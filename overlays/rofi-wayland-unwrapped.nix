c501f713a5774790c20c9e84b6370348dbf4581b
Donovan Glover <donovan@dglover.co>
2024-09-01 14:20:51 -0400

    overlays(rofi): Prefer fetchpatch where possible

--- overlays/rofi-wayland-unwrapped.nix
+++ overlays/rofi-wayland-unwrapped.nix
@@ -1,5 +1,10 @@
 final: prev: {
   rofi-wayland-unwrapped = prev.rofi-wayland-unwrapped.overrideAttrs (oldAttrs: {
-    patches = (oldAttrs.patches or [ ]) ++ [ ../assets/rofi-wayland-fix-touchpad-scrolling.patch ];
+    patches = (oldAttrs.patches or [ ]) ++ [
+      (prev.fetchpatch {
+        url = "https://github.com/lbonn/rofi/commit/93ad86da10b1747f4e584bc6bfbfbc3eddb280a3.patch";
+        hash = "sha256-COWlVTa/C8oySxLdNo8w3W5WiujGIr/cDfXQWRkI1yw=";
+      })
+    ];
   });
 }
