4c301830d52ea3c575740c52b0f2ea1cf9b31a40
Donovan Glover <donovan@dglover.co>
2024-09-02 11:03:35 -0400

    packages(stylix-background): Add missing meta attributes

--- packages/stylix-background.nix
+++ packages/stylix-background.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   stdenvNoCC,
   imagemagick,
   color ? "181818",
@@ -16,4 +17,12 @@ stdenvNoCC.mkDerivation {
     mkdir -p $out
     magick -size 1x1 xc:#${color} $out/wallpaper.png
   '';
+
+  meta = {
+    description = "Solid background color for stylix to use";
+    license = lib.licenses.publicDomain;
+    homepage = "https://stackoverflow.com/questions/7771975/imagemagick-create-a-png-file-which-is-just-a-solid-rectangle";
+    maintainers = with lib.maintainers; [ donovanglover ];
+    platforms = lib.platforms.all;
+  };
 }
