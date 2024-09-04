0a7ac98d82d3575f9c205e09330e2764e06976e9
Donovan Glover <donovan@dglover.co>
2024-09-03 23:35:49 -0400

    meta: Move batsignal to separate module

---
+++ home/batsignal.nix
@@ -0,0 +1,3 @@
+{
+  services.batsignal.enable = true;
+}
--- home/hyprland.nix
+++ home/hyprland.nix
@@ -335,7 +335,6 @@ in
   };
 
   services = {
-    batsignal.enable = true;
     swaync.enable = true;
     hyprpaper.enable = mkForce false;
   };
