58e227efc01c367b1476f4c7f94d7641007a5190
Donovan Glover <donovan@dglover.co>
2024-09-08 19:09:10 -0400

    phone: Move dconf config to dconf module

--- hosts/phone/configuration.nix
+++ hosts/phone/configuration.nix
@@ -8,7 +8,6 @@
 
 let
   inherit (lib) mkIf mkForce;
-  inherit (lib.gvariant) mkTuple mkUint32;
   inherit (config.modules.system) username;
   inherit (builtins) attrValues;
 
@@ -32,6 +31,7 @@ in
 
   home-manager.sharedModules = attrValues {
     inherit (self.homeModules)
+      dconf
       eza
       fish
       git
@@ -100,68 +100,6 @@ in
           music = mkForce "/home/${username}";
           pictures = mkForce "/home/${username}";
           videos = mkForce "/home/${username}";
-        };
-      };
-
-      dconf.settings = {
-        "sm/puri/phosh" = {
-          app-filter-mode = [ ];
-          enable-suspend = true;
-
-          favorites = [
-            "org.gnome.Calls.desktop"
-            "sm.puri.Chatty.desktop"
-            "kitty.desktop"
-            "librewolf.desktop"
-          ];
-        };
-
-        "sm/puri/phosh/lockscreen" = {
-          shuffle-keypad = true;
-        };
-
-        "sm/puri/phosh/plugins" = {
-          quick-settings = [
-            "night-light-quick-setting"
-            "caffeine-quick-setting"
-          ];
-        };
-
-        "org/gnome/desktop/interface" = {
-          show-battery-percentage = true;
-        };
-
-        "org/postmarketos/megapixels" = {
-          save-raw = false;
-          postprocessor = "${pkgs.megapixels}/share/megapixels/postprocess.sh";
-        };
-
-        "org/gnome/desktop/session" = {
-          idle-delay = mkUint32 60;
-        };
-
-        "org/gnome/desktop/input-sources" = {
-          sources = [
-            (mkTuple [
-              "xkb"
-              "us"
-            ])
-
-            (mkTuple [
-              "xkb"
-              "jp+kana"
-            ])
-          ];
-        };
-
-        "org/gnome/eog/ui" = {
-          image-gallery = true;
-          sidebar = false;
-        };
-
-        "org/gnome/settings-daemon/plugins/power" = {
-          sleep-inactive-ac-type = "nothing";
-          sleep-inactive-battery-type = "suspend";
         };
       };
     };
