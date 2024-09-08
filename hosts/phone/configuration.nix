5df2225472abe15c0c5271eddffb6f0e9b11411e
Donovan Glover <donovan@dglover.co>
2024-09-08 19:29:33 -0400

    phone: Move pinephone video script to separate package
    Now it's possible to use the video recording script outside of fish.

--- hosts/phone/configuration.nix
+++ hosts/phone/configuration.nix
@@ -47,30 +47,7 @@ in
       ;
 
     config = {
-      programs = {
-        fish.functions.video = # fish
-          ''
-            ${pkgs.v4l-utils}/bin/media-ctl -d1 --links '"gc2145 3-003c":0->1:0[0],"ov5640 3-004c":0->1:0[1]'
-            ${pkgs.v4l-utils}/bin/media-ctl -d1 --set-v4l2 '"ov5640 3-004c":0[fmt:UYVY8_2X8/1280x720@1/30]'
-
-            ffmpeg \
-              -input_format yuv420p \
-              -s 1280x720 \
-              -f video4linux2 \
-              -thread_queue_size 4096 \
-              -i /dev/video1 \
-              -f pulse \
-              -thread_queue_size 256 \
-              -i alsa_input.platform-sound.Voice_Call__DigitalMic__source \
-              -c:a libopus \
-              -c:v libx264 \
-              -preset ultrafast \
-              -qp 23 \
-              "$(date '+%x（%a）%R').mp4"
-          '';
-
-        man.generateCaches = mkForce false;
-      };
+      programs.man.generateCaches = mkForce false;
 
       xdg = {
         desktopEntries = {
