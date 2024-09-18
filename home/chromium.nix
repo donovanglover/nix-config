4b12396bf4c2910e896159c38f7c94b859ff1317
Donovan Glover <donovan@dglover.co>
2024-09-18 14:29:23 -0400

    chromium: Remove wayland flags
    This caused a bunch of horrible to debug issues on Hyprland/Wayland such
    as tab switching not working. This also broke the usage of Chromium in
    non-Wayland environments like dwm.

--- home/chromium.nix
+++ home/chromium.nix
@@ -6,8 +6,6 @@
     package = pkgs.ungoogled-chromium;
 
     commandLineArgs = [
-      "--ozone-platform=wayland"
-      "--enable-wayland-ime"
       "--extension-mime-request-handling=always-prompt-for-install"
       "--webrtc-ip-handling-policy=default_public_interface_only"
     ];
