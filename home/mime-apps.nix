5d5c4d78533d38a6e43deb42d283cca69c823a61
Donovan Glover <donovan@dglover.co>
2024-10-10 09:21:26 -0400

    mime-apps: specify text/html and application/pdf
    Necessary due to Chromium wanting to open these files instead.

--- home/mime-apps.nix
+++ home/mime-apps.nix
@@ -3,11 +3,13 @@
     enable = true;
 
     defaultApplications = {
+      "text/html" = "librewolf.desktop";
       "text/markdown" = "nvim.desktop";
       "text/plain" = "nvim.desktop";
       "image/png" = "pqiv.desktop";
       "image/jpeg" = "pqiv.desktop";
       "image/gif" = "org.qutebrowser.qutebrowser.desktop";
+      "application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
       "application/x-wine-extension-osz" = "osu-stable.desktop";
       "x-scheme-handler/http" = "librewolf.desktop";
       "x-scheme-handler/https" = "librewolf.desktop";
