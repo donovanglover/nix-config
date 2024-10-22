4c45006c76f7efe73ea94d4cdf09c28385561a8c
Donovan Glover <donovan@dglover.co>
2024-10-22 05:38:21 -0400

    containers(wine): remove unused wayland session variables

--- modules/containers.nix
+++ modules/containers.nix
@@ -97,11 +97,6 @@ in
 
             sessionVariables = {
               WAYLAND_DISPLAY = "wayland-1";
-              QT_QPA_PLATFORM = "wayland";
-              QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-              SDL_VIDEODRIVER = "wayland";
-              CLUTTER_BACKEND = "wayland";
-              MOZ_ENABLE_WAYLAND = "1";
               XDG_RUNTIME_DIR = "/run/user/1000";
               DISPLAY = ":0";
               QT_IM_MODULE = "fcitx";
