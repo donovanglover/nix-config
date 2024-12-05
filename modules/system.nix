6de52cc0c6bc46c434030cdc0acdb37bd2de1e96
Donovan Glover <donovan@dglover.co>
2024-12-04 20:30:56 -0500

    system: add udev rule for numen

--- modules/system.nix
+++ modules/system.nix
@@ -167,6 +167,7 @@ in
               "dialout"
               "feedbackd"
               "video"
+              "input"
             ];
       };
     };
@@ -264,6 +265,10 @@ in
           KbdInteractiveAuthentication = false;
         };
       };
+
+      udev.extraRules = ''
+        KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"
+      '';
     };
 
     environment = {
