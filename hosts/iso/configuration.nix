4a0445454728d9edc68a5b132ef72ac342f9540e
Donovan Glover <donovan@dglover.co>
2025-01-27 12:41:47 -0500

    meta: document working custom iso
    This *worked* and was kind of cool, but I realized that I don't actually
    need a custom iso due to familiarity with the tty and access to any
    program from any flake with Nix.

--- hosts/iso/configuration.nix
+++ hosts/iso/configuration.nix
@@ -1,13 +1,48 @@
-{ pkgs, modulesPath, ... }:
+{
+  pkgs,
+  lib,
+  modulesPath,
+  nix-config,
+  ...
+}:
 
+let
+  inherit (builtins) attrValues;
+
+  username = "nixos";
+in
 {
   imports = [
     (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
-  ];
 
-  environment.systemPackages = with pkgs; [
-    neovim
-  ];
+    nix-config.inputs.home-manager.nixosModules.home-manager
+  ] ++ (with nix-config.nixosModules; [
+    stylix
+    fonts
+    desktop
+    librewolf
+    shell
+  ]);
+
+  nixpkgs.overlays = attrValues nix-config.overlays;
+  environment.systemPackages = attrValues nix-config.packages.${pkgs.system};
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+
+    sharedModules =
+      attrValues nix-config.homeModules
+      ++ lib.singleton {
+        home.stateVersion = "24.11";
+      };
+
+    users.${username}.home = {
+      inherit username;
+
+      homeDirectory = "/home/${username}";
+    };
+  };
 
   isoImage.squashfsCompression = "gzip -Xcompression-level 1";
 }
