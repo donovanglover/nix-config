63391b634adfc0d33aa062e496cb8907c075d8a3
Donovan Glover <donovan@dglover.co>
2025-01-27 13:13:15 -0500

    chore: drop phosh-backgrounds
    Missed in the phone removal commit.

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,17 +1,5 @@
-{
-  nixosConfig,
-  config,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 
-let
-  inherit (nixosConfig._module.specialArgs) nix-config;
-
-  phosh-backgrounds = nix-config.packages.${pkgs.system}.phosh-backgrounds.override {
-    color = config.lib.stylix.colors.base00;
-  };
-in
 {
   gtk = {
     enable = true;
@@ -30,28 +18,4 @@ in
       name = "Fluent-dark";
     };
   };
-
-  stylix.targets.gtk.extraCss = # css
-    ''
-      phosh-lockscreen {
-        background: url('${phosh-backgrounds}/wall-lock.jpg');
-      }
-
-      phosh-app-grid {
-        background: url('${phosh-backgrounds}/wall-grid.jpg');
-      }
-
-      phosh-top-panel {
-        background: url('${phosh-backgrounds}/wall-panel.jpg');
-      }
-
-      phosh-home {
-        background: url('${phosh-backgrounds}/wall-home.jpg');
-      }
-
-      phosh-lockscreen, phosh-app-grid, phosh-top-panel, phosh-home {
-        background-size: cover;
-        background-position: center;
-      }
-    '';
 }
