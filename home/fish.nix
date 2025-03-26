b9e5dbe4a45fa43dcf2ec95632a106c438bed67d
Donovan Glover <donovan@dglover.co>
2025-03-26 08:10:56 -0400

    fish(ffmpeg): handle files with colons properly

--- home/fish.nix
+++ home/fish.nix
@@ -130,7 +130,7 @@
         ''
           set ORIGINAL_SIZE (du -hs | cut -f1)
 
-          fd -e wav -x ffmpeg -i "{}" -loglevel quiet -stats "{.}.flac"
+          fd -e wav -x ffmpeg -i "file:{}" -loglevel quiet -stats "file:{.}.flac"
           fd -e wav -X trash
 
           set NEW_SIZE (du -hs | cut -f1)
@@ -142,7 +142,7 @@
         ''
           set ORIGINAL_SIZE (du -hs | cut -f1)
 
-          fd -e wav -e flac -x ffmpeg -i "{}" -c:a libopus -b:a 128K -loglevel quiet -stats "{.}.opus"
+          fd -e wav -e flac -x ffmpeg -i "file:{}" -c:a libopus -b:a 128K -loglevel quiet -stats "file:{.}.opus"
           fd -e wav -e flac -X rm -I
 
           set NEW_SIZE (du -hs | cut -f1)
