fcf3a638b1b3de941aa45fd416ce2a0cd624c3bf
Donovan Glover <donovan@dglover.co>
2025-06-03 17:50:37 -0400

    hypridle: remove systemctl suspend
    I always ended up disabling hypridle to prevent auto-suspend from
    occurring, so it makes sense to remove this.

--- home/hypridle.nix
+++ home/hypridle.nix
@@ -24,10 +24,6 @@
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
-        {
-          timeout = 1800;
-          on-timeout = "systemctl suspend";
-        }
       ];
     };
   };
