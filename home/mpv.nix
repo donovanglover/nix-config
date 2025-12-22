863f5c3ae333aeb462f72b492a96a33b89fc36ac
Donovan Glover <donovan@dglover.co>
2025-12-22 12:38:22 -0500

    mpv: use sane screenshot filenames
    Avoids an issue where screenshots were indistinguishable due to all of
    them being some variation of mpv-shot-XXXX.png. Screenshots now have
    context with the filename of the video and the timestamp it was taken.

--- home/mpv.nix
+++ home/mpv.nix
@@ -33,6 +33,7 @@ in
       fullscreen = true;
 
       screenshot-format = "png";
+      screenshot-template = "%F-%p-%n";
 
       title = "\${filename} - mpv";
       script-opts = "osc-title=\${filename},osc-boxalpha=150,osc-visibility=never,osc-boxvideo=yes";
