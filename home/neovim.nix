9da20159f3c520d78e74cbed62a6e5976cf8c994
Donovan Glover <donovan@dglover.co>
2026-04-22 15:23:31 -0400

    neovim: remove old lsp.with
    See https://github.com/neovim/neovim/discussions/32045
    
    Removing lsp.with entirely keeps things simple.

--- home/neovim.nix
+++ home/neovim.nix
@@ -196,14 +196,6 @@
               end,
             })
 
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-              vim.lsp.handlers.hover, { border = "single" }
-            )
-
-            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-              vim.lsp.handlers.signature_help, { border = "single" }
-            )
-
             vim.diagnostic.config {
               float = { border = "single" }
             }
